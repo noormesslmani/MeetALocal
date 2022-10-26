56fe9f823cf71f469655103a156593b6851e3598
noormesslmani <noor.messlmani@gmail.com>
2022-10-26 19:24:11 +0300

    Backend: created AuthController

--- MeetALocal-Backend/routes/api.php
+++ MeetALocal-Backend/routes/api.php
@@ -4,20 +4,34 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocalController;
 use App\Http\Controllers\ForeignerController;
 use App\Http\Controllers\AdminController;
-
+use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v1.0.0'], function () {
-    Route::group(['prefix' => 'local'], function () {
-    
-    });
-    Route::group(['prefix' => 'admin'], function () {
-    
-    });
-    Route::group(['prefix' => 'foreigner'], function () {
-    
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['prefix' => 'local'], function () {
+        
+        });
+        Route::group(['prefix' => 'admin'], function () {
+        
+        });
+        Route::group(['prefix' => 'foreigner'], function () {
+        
+        });
+        Route::group(['prefix' => 'auth'], function ($router) { 
+            Route::post('/logout', [AuthController::class, 'logout']);
+            Route::post('/refresh', [AuthController::class, 'refresh']);
+            Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+        });
     });
 
+    Route::group(['prefix' => 'auth'], function ($router) {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);  
+    }); 
 });
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
