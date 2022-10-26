11520fb21b8adcacc7d1ac3a31cf8db2b2275757
noormesslmani <noor.messlmani@gmail.com>
2022-10-27 00:17:39 +0300

    Backend: created an api to retrieve locals

--- MeetALocal-Backend/app/Http/Controllers/AuthController.php
+++ MeetALocal-Backend/app/Http/Controllers/AuthController.php
@@ -23,10 +23,6 @@ use Validator;
 class AuthController extends Controller
 {
     
-    // public function __construct() {
-    //     $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    // }
-    
     public function login(Request $request){
     	$validator = Validator::make($request->all(), [
             'email' => 'required|email',
--- MeetALocal-Backend/routes/api.php
+++ MeetALocal-Backend/routes/api.php
@@ -4,13 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LocalController;
 use App\Http\Controllers\ForeignerController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v1.0.0'], function () {
 
     Route::group(['middleware' => 'auth:api'], function () {
+        Route::group(['prefix' => 'user'], function () {
+            Route::get('/locals/{country?}{fees?}', [UserController::class, 'getLocals']);
+        });
         Route::group(['prefix' => 'local'], function () {
-        
+            
         });
         Route::group(['prefix' => 'admin'], function () {
         
