fe7245b4f4e400b45dcb671eb997ac57751d198b
noormesslmani <noor.messlmani@gmail.com>
2022-10-26 15:55:33 +0300

    Backend: prepared routes groups and some controllers

--- MeetALocal-Backend/routes/api.php
+++ MeetALocal-Backend/routes/api.php
@@ -1,18 +1,23 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocalController;
+use App\Http\Controllers\ForeignerController;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+Route::group(['prefix' => 'v1.0.0'], function () {
+    Route::group(['prefix' => 'local'], function () {
+    
+    });
+    Route::group(['prefix' => 'admin'], function () {
+    
+    });
+    Route::group(['prefix' => 'foreigner'], function () {
+    
+    });
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
