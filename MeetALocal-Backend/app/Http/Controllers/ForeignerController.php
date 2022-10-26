d0c8a9d0d4871a3cf4766d780961d64d3d329321
noormesslmani <noor.messlmani@gmail.com>
2022-10-26 15:55:33 +0300

    Backend: prepared routes groups and some controllers

---
+++ MeetALocal-Backend/app/Http/Controllers/ForeignerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Country;
+use App\Models\Event_category;
+use App\Models\Event;
+use App\Models\Favorite_local;
+use App\Models\Highlight;
+use App\Models\Local_category;
+use App\Models\Message;
+use App\Models\Notification;
+use App\Models\PostCategory;
+use App\Models\Post;
+use App\Models\Saved_event;
+use App\Models\User_type;
+use Illuminate\Http\Request;
+
+class ForeignerController extends Controller
+{
+    //
+}
