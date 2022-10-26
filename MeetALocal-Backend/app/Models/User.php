98b52503d6ae17795e03784ee16461f3ddded5b3
noormesslmani <noor.messlmani@gmail.com>
2022-10-26 13:36:27 +0300

    Backend: created model for each table

--- MeetALocal-Backend/app/Models/User.php
+++ MeetALocal-Backend/app/Models/User.php
@@ -12,32 +12,26 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'nationality_id',
+        'residence_id',
+        'gender',
+        'phone',
+        'date_of_birth',
+        'about',
+        'location',
+        'profile_picture',
+        'fees'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
