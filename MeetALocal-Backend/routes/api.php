9ba18b16e900d9b0faffc41cdbeb5949c746a85a
noormesslmani <noor.messlmani@gmail.com>
2022-10-27 23:25:55 +0300

    Backend: created get bans api for admins

--- MeetALocal-Backend/routes/api.php
+++ MeetALocal-Backend/routes/api.php
@@ -30,6 +30,7 @@ Route::group(['prefix' => 'v1.0.0'], function () {
         Route::group(['prefix' => 'admins'], function () {
             Route::post('/ban', [AdminController::class, 'banUser']);
             Route::post('/unban', [AdminController::class, 'unbanUser']);
+            Route::get('/get-bans', [AdminController::class, 'getBans']);
         });
         Route::group(['prefix' => 'foreigners'], function () {
         
