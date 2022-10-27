0087e1c26f55b97b0998dac3f05ff21c53952494
noormesslmani <noor.messlmani@gmail.com>
2022-10-27 15:08:43 +0300

    Backend: gathered post categories in a single array in get posts api

--- MeetALocal-Backend/app/Http/Controllers/UserController.php
+++ MeetALocal-Backend/app/Http/Controllers/UserController.php
@@ -67,10 +67,14 @@ class UserController extends Controller
     public function getPosts($country, $category){
         $country!='all'? $country_id= Country::where('country',$country)->pluck('id'):$country_id=Country::pluck('id');
         $category!='all'? $category_id=Category::where('category',$category)->pluck('id'):$category_id=Category::pluck('id');
-        $data= Post::join('post_categories','posts.id','=','post_id')->join('categories','post_categories.category_id','=','categories.id')->join('countries','posts.country_id','=','countries.id')->join('users','posts.user_id','=','users.id')->whereIn('posts.country_id',$country_id)->whereIn('post_categories.category_id',$category_id)->orderBy('posts.id', 'desc')->select('posts.*','post_categories.category_id','categories.category','countries.country','users.name')->distinct()->get();
+        $posts= Post::join('post_categories','posts.id','=','post_id')->join('categories','post_categories.category_id','=','categories.id')->join('countries','posts.country_id','=','countries.id')->join('users','posts.user_id','=','users.id')->whereIn('posts.country_id',$country_id)->whereIn('post_categories.category_id',$category_id)->orderBy('posts.id', 'desc')->select('posts.*','countries.country','users.name')->distinct()->get();
+        foreach($posts as $post){
+            $category= $post->categories()->pluck('category');
+            $post['categories']=$category;
+        }
         return response()->json([
             'message' => 'ok',
-            'data' => $data,
+            'data' => $posts,
         ], 201);
     }
     public function createPost(Request $request){
