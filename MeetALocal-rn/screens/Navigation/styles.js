45ce4382c57eabbac3e7d3ca5e3ec81d6d45b17d
noormesslmani <noor.messlmani@gmail.com>
2022-10-29 02:04:19 +0300

    frontend: created login form

--- MeetALocal-rn/screens/Navigation/styles.js
+++ MeetALocal-rn/screens/Navigation/styles.js
@@ -1,6 +1,16 @@
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions} from "react-native";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
-    
+    container:{
+        justifyContent:"center",
+        alignItems:"center",
+        width: windowWidth,
+        paddingRight:25
+    },
+    logo:{
+        width: 300, 
+        height: 100
+    }
 })
+export default styles;
