ae9f881d45caf74289e6ebcdfa276968937a920f
noormesslmani <noor.messlmani@gmail.com>
2022-10-28 23:16:45 +0300

    frontend: prepared some screens

---
+++ MeetALocal-rn/screens/Auth/SigninScreen.js
@@ -0,0 +1,14 @@
+import { View, Text, Modal, Button } from 'react-native'
+import React from 'react'
+import styles from './styles';
+import LoginForm from '../../components/LoginForm';
+const SigninScreen= ()=> {
+  return (
+    <View style={styles.background}>
+      
+        <Text>Hello</Text>
+  
+    </View>
+  )
+}
+export default SigninScreen
---
+++ MeetALocal-rn/screens/Navigation/Logo.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Logo= ()=> {
+    return (
+        <View>
+            <Image
+            style={{ width: 300, height: 100, alignSelf: "center", }}
+            source={require('../../assets/logo.png')}/>
+        </View>
+    )
+  }
+  export default Logo
