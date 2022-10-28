12c484519cfaa958661ee3648c535e59f54643b1
noormesslmani <noor.messlmani@gmail.com>
2022-10-28 23:18:18 +0300

    frontend: prepared some components

---
+++ MeetALocal-rn/components/AuthButton.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+export default function AuthButton() {
+  return (
+    <View>
+      <Text>AuthButton</Text>
+    </View>
+  )
+}
---
+++ MeetALocal-rn/components/LoginForm.js
@@ -0,0 +1,12 @@
+import { View, Text, Modal } from 'react-native'
+import React from 'react'
+
+export default function LoginForm() {
+  return (
+    <View>
+      <Modal>
+        <Text>Hello</Text>
+      </Modal>
+    </View>
+  )
+}
