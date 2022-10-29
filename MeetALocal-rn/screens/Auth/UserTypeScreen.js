2502b572479f71403866997f0cd4218ce1aa4d3a
noormesslmani <noor.messlmani@gmail.com>
2022-10-29 12:49:14 +0300

    Frontend: added some styling elements

--- MeetALocal-rn/screens/Auth/UserTypeScreen.js
+++ MeetALocal-rn/screens/Auth/UserTypeScreen.js
@@ -6,18 +6,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const UserTypeScreen=({navigation})=> {
   const [type, setType]=useState('');
-  const handleSubmit=()=>{
-    console.log('submitted')
-  }
+  console.log(type)
   return (
     <View style={styles.backgroundUserType}>
         <Text style={styles.welcome}>Welcome</Text>
         <View style={[styles.formContainer, styles.shadowProp, styles.userType]}>
           <Text style={styles.signIn}>I am?</Text>
-          <TouchableOpacity style={styles.typeBtn} onPress={handleSubmit}>
+          <TouchableOpacity style={styles.typeBtn} onPress={()=>{setType('Local')}}>
             <Text>Local</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.typeBtn} onPress={handleSubmit}>
+          <TouchableOpacity style={styles.typeBtn} onPress={()=>{setType('Foreigner')}}>
             <Text>Foreigner</Text>
           </TouchableOpacity>
         </View>
