ff77adc45abf7eca70a3b0c77d4b90236dc409d3
noormesslmani <noor.messlmani@gmail.com>
2022-10-29 12:57:22 +0300

    Frontend: modified some text input fields in signup page

--- MeetALocal-rn/screens/Auth/UserTypeScreen.js
+++ MeetALocal-rn/screens/Auth/UserTypeScreen.js
@@ -6,16 +6,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const UserTypeScreen=({navigation})=> {
   const [type, setType]=useState('');
+  const handleLocal=()=>{
+    setType('Local')
+  }
+  const handleForeigner=()=>{
+    setType('Foreigner')
+  }
   console.log(type)
   return (
     <View style={styles.backgroundUserType}>
         <Text style={styles.welcome}>Welcome</Text>
         <View style={[styles.formContainer, styles.shadowProp, styles.userType]}>
           <Text style={styles.signIn}>I am?</Text>
-          <TouchableOpacity style={styles.typeBtn} onPress={()=>{setType('Local')}}>
+          <TouchableOpacity style={styles.typeBtn} onPress={handleLocal}>
             <Text>Local</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.typeBtn} onPress={()=>{setType('Foreigner')}}>
+          <TouchableOpacity style={styles.typeBtn} onPress={handleForeigner}>
             <Text>Foreigner</Text>
           </TouchableOpacity>
         </View>
