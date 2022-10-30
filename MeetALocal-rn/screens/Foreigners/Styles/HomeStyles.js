4de44db62a1ca7f99be8f7c8e51b4803033b6a4d
noormesslmani <noor.messlmani@gmail.com>
2022-10-31 01:18:09 +0200

    Frontend: set profile picture in home page and created card component

--- MeetALocal-rn/screens/Foreigners/Styles/HomeStyles.js
+++ MeetALocal-rn/screens/Foreigners/Styles/HomeStyles.js
@@ -11,6 +11,24 @@ const HomeStyles = StyleSheet.create({
         fontSize:45,
         color:"#8C57BA",
     },
+    name:{
+        fontSize:20,
+        margin:5,
+    },
+    photo:{
+        width: 0.5*windowWidth,
+        height: 0.5*windowWidth,
+        borderRadius: 0.25*windowWidth,
+        margin:20
+    },
+    cardContainer:{
+        alignItems:"center",
+        width: 0.8*windowWidth,
+        height: 0.2*windowHeight,
+    },
+    card:{
+        backgroundColor:"rgba(140, 87, 186, 0.34)"
+    }
 });
 
 export default HomeStyles;
