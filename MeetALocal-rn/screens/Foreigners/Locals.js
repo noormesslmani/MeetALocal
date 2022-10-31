74cbf10c198d0c0102bf5d88bd5070d056d3127a
noormesslmani <noor.messlmani@gmail.com>
2022-10-31 03:30:56 +0200

    Frontend: implemented screen navigation upon press on cards in home page

---
+++ MeetALocal-rn/screens/Foreigners/Locals.js
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import HomeStyles from './Styles/HomeStyles';
+
+const Locals=({navigation})=> {
+  return (
+    <View style={HomeStyles.container}>
+        <Text>Locals page</Text>
+    </View>
+  )
+}
+export default Locals
