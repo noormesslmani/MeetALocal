45f15aa10fa53a06f296dfeb5b0f9622e6d7d5cb
noormesslmani <noor.messlmani@gmail.com>
2022-10-31 20:10:40 +0200

    Frontend: styled the local card

---
+++ MeetALocal-rn/components/ComponentsStyles/LocalCardStyles.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const LocalCardStyles = StyleSheet.create({
+    card:{
+        margin:50,
+    },
+    image:{
+        borderTopLeftRadius:5,
+        borderTopRightRadius:5,
+        width: 0.75*windowWidth,
+        height: 0.3*windowHeight,
+        position: "relative"
+    },
+    infoContainer:{
+        width: 0.75*windowWidth,
+        height: 0.07*windowHeight,
+        backgroundColor:"rgba(217, 217, 217, 0.75)",
+        position:'absolute',
+        top: 0.23*windowHeight,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    name:{
+        fontSize:15,
+        fontWeight:"500"
+    },
+    country:{
+        fontSize:12
+    },
+    likes:{
+        fontSize:10
+    },
+    fees:{
+        position:'absolute',
+        right:0.02*windowWidth,
+        top: 0.25*windowHeight
+    },
+    categoryContainer:{
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"center",
+        width: 0.75*windowWidth,
+        height: 0.06*windowHeight,
+        backgroundColor:"rgba(140, 87, 186, 0.34)",
+        borderBottomLeftRadius:5,
+        borderBottomRightRadius:5
+    }
+   
+});
+export default LocalCardStyles;
