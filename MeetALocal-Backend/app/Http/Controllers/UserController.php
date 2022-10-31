3e66feb769820b338475beda4039eb88da2f1a71
noormesslmani <noor.messlmani@gmail.com>
2022-11-01 01:10:59 +0200

    Backend: removed filter by fees option from get locals api

--- MeetALocal-Backend/app/Http/Controllers/UserController.php
+++ MeetALocal-Backend/app/Http/Controllers/UserController.php
@@ -21,11 +21,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
-    public function getLocals($country, $fees, $category){
+    public function getLocals($country, $category){
         $country!='all'? $country_id= Country::where('country',$country)->pluck('id'):$country_id=Country::pluck('id');
-        $fees=='all'? $fees=100:$fees='all';
         $category!='all'? $category_id=Category::where('category',$category)->pluck('id'):$category_id=Category::pluck('id');
-        $locals= User::join('local_categories','users.id','=','local_id')->join('categories','local_categories.category_id','=','categories.id')->join('countries','users.residence_id','=','countries.id')->where('type_id',1)->where('users.fees','<=',$fees)->whereIn('users.residence_id',$country_id)->whereIn('local_categories.category_id',$category_id)->select('users.*','countries.country')->distinct()->inRandomOrder()->get();
+        $locals= User::join('local_categories','users.id','=','local_id')->join('categories','local_categories.category_id','=','categories.id')->join('countries','users.residence_id','=','countries.id')->where('type_id',1)->whereIn('users.residence_id',$country_id)->whereIn('local_categories.category_id',$category_id)->select('users.*','countries.country')->distinct()->inRandomOrder()->get();
         foreach($locals as $local){
             $category= $local->categories()->pluck('category');
             $likes= FavoriteLocal::where('local_id',$local->id)->count();
