8d12eabd261907c51861a25a399056a8ef68a41d
noormesslmani <noor.messlmani@gmail.com>
2022-11-01 01:30:48 +0200

    Frontend; implemented get events api

--- MeetALocal-rn/screens/Foreigners/Events.js
+++ MeetALocal-rn/screens/Foreigners/Events.js
@@ -3,12 +3,43 @@ import React from 'react'
 import HomeStyles from './Styles/HomeStyles';
 import { useState, useEffect, useContext } from "react";
 import EventsStyles from './Styles/EventsPageStyles';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
 import FilterModal from '../../components/Home/FilterModal';
 const Events=({navigation})=> {
   const [viewSaved, setViewSaved]=useState(false)
   const [modalVisible, setModalVisible] = useState(false)
   const [country, setCountry]=useState('all');
   const [category, setCategory]=useState('all');
+  const [data, setdata]=useState([])
+
+  const renderItem = ({ item }) => (
+    <View style={{flexDirection:"row"}}>
+      <EventCard item={item} />
+      <EventCard item={item} />
+    </View>
+  )
+  useEffect(()=>{
+    if(!viewSaved){
+      getEvents()}
+  },[viewSaved, country, category])
+  
+  async function getEvents(){
+    const token = await AsyncStorage.getItem('@token')
+    axios({
+      method: "get",
+      headers: { Authorization: `Bearer ${token}`},
+      url:`http://192.168.1.7:8000/api/v1.0.0/users/events/${country}/${category}`,
+    })
+    .then((response)=> {
+      console.log(response.data.data)
+      setdata(response.data.data)
+      return response;
+    })
+    .catch(function (error) {
+      console.warn(error)
+    });
+  }
   return (
     <View style={HomeStyles.container}>
         <Text style={EventsStyles.title}>Events</Text>
