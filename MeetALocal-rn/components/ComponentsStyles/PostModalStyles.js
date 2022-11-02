a5b64be3620a2d648cca186ffda7d3bc09cc5364
noormesslmani <noor.messlmani@gmail.com>
2022-11-02 02:07:15 +0200

    Frontend: added some style and structure to new post modal

--- MeetALocal-rn/components/ComponentsStyles/PostModalStyles.js
+++ MeetALocal-rn/components/ComponentsStyles/PostModalStyles.js
@@ -23,8 +23,46 @@ const PostModalStyles = StyleSheet.create({
         },
         shadowOpacity: 0.25,
         shadowRadius: 4,
-        elevation: 5
+        elevation: 5,
+        padding:20
       },
+      title:{
+        fontSize:26,
+        color:"#8C57BA"
+      },
+      contentContainer:{
+        marginTop:"10%",
+        width:"90%"
+      },
+      input:{
+        marginTop:20,
+        height:100,
+        width:"100%",
+        borderBottomWidth:0.5,
+        borderColor:"#4BB0F9",
+        marginBottom:40
+      },
+      optionsContainer:{
+        marginTop:10,
+        borderTopWidth:0,
+        borderLeftWidth:0,
+        borderRightWidth:0,
+        borderBottomRightRadius:0,
+        borderBottomLeftRadius:0,
+        borderBottomColor:"#4BB0F9",
+        borderBottomWidth:0.5
+      },
+      button: {
+        borderRadius: 20,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 10,
+        paddingBottom: 10,
+        elevation: 1,
+        marginTop:60,
+        backgroundColor:"rgba(75, 176, 249, 0.5)"
+      },
+
 
 });
 export default PostModalStyles;
