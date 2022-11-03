9dff220598e981f98041def96046c8b95d6dae1c
noormesslmani <noor.messlmani@gmail.com>
2022-11-03 22:39:31 +0200

    Frontend: set about textinput limit to 200 characters

--- MeetALocal-rn/screens/General/Posts.js
+++ MeetALocal-rn/screens/General/Posts.js
@@ -66,13 +66,14 @@ const Posts=({navigation})=> {
             <TouchableOpacity onPress={()=>setViewOwn(false)} >{ <Text style={[PostsStyles.options,viewOwn? null: PostsStyles.selected ]}>All Posts</Text>}</TouchableOpacity>
             <TouchableOpacity onPress={()=>setViewOwn(true)}>{ <Text style={[PostsStyles.options, viewOwn? PostsStyles.selected: null]}>My Posts</Text>}</TouchableOpacity>
         </View>
-        {!viewOwn && <TouchableOpacity onPress={()=>{setModalVisible(true)}}><Text style={{color:'grey', marginBottom:5}}>Filter</Text></TouchableOpacity>}
-        <FilterModal modalVisible={modalVisible} setModalVisible={setModalVisible} setCountry={setCountry} setCategory={setCategory}/>
-        <View style={PostsStyles.separator}/>
 
+        <FilterModal modalVisible={modalVisible} setModalVisible={setModalVisible} setCountry={setCountry} setCategory={setCategory}/>
         <TouchableOpacity onPress={()=>{setNewPostModalVisible(true)}}>
           <Text style={PostsStyles.newPost}>New Post</Text>
         </TouchableOpacity>
+        <View style={PostsStyles.separator}/>
+        {!viewOwn && <TouchableOpacity onPress={()=>{setModalVisible(true)}}><Text style={{color:'grey', margin:10}}>Filter</Text></TouchableOpacity>}
+        
         <NewPostModal modalVisible={newPostModalVisible} setModalVisible={setNewPostModalVisible}/>
         <SafeAreaView>
           <FlatList
