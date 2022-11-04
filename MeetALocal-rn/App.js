ecede67032a6f5b2e7b9707a3383e1423c511d06
noormesslmani <noor.messlmani@gmail.com>
2022-11-04 15:34:56 +0200

    Frontend: added locals into context provider

--- MeetALocal-rn/App.js
+++ MeetALocal-rn/App.js
@@ -4,8 +4,9 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 export default function App() {
   const [user,setUser]=useState({})
+  const [locals,setLocals]=useState([])
   return(
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider value={{user, setUser, locals, setLocals}}>
       <RootNavigation/>
     </UserContext.Provider>
   )
