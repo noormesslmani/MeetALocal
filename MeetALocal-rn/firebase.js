aca953a764a1a7d6e9f8ff21bba0bbacd911a1a1
noormesslmani <noor.messlmani@gmail.com>
2022-11-04 17:37:38 +0200

    Frontend: installed firebase

---
+++ MeetALocal-rn/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeMcsoBWWDx64KtmHAeo0jneKTDbQWHo8",
+  authDomain: "meetalocal-6d2ec.firebaseapp.com",
+  projectId: "meetalocal-6d2ec",
+  storageBucket: "meetalocal-6d2ec.appspot.com",
+  messagingSenderId: "529881386450",
+  appId: "1:529881386450:web:e33a9bb4c5faed16738c0b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
