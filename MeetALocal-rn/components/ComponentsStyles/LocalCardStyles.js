a426fce99129ef8659e67179175d13c799502691
noormesslmani <noor.messlmani@gmail.com>
2022-11-04 15:17:53 +0200

    Frontend: displayed categories on locals card

--- MeetALocal-rn/components/ComponentsStyles/LocalCardStyles.js
+++ MeetALocal-rn/components/ComponentsStyles/LocalCardStyles.js
@@ -10,19 +10,18 @@ const LocalCardStyles = StyleSheet.create({
     },
     card:{
         width: 0.9*windowWidth,
-        height: 0.4*windowHeight,
         borderRadius:10,
         shadowColor: "#000",
         shadowOffset: {
             width: 2,
             height: 2,
         },
-        shadowOpacity: 0.1,
+        shadowOpacity: 0.5,
         shadowRadius: 5,
-        elevation: 5,
     },
     image:{
-        borderRadius:10,
+        borderTopEndRadius:10,
+        borderTopRightRadius:10,
         width: 0.9*windowWidth,
         height: 0.4*windowHeight,
         position: "relative",
@@ -30,9 +29,9 @@ const LocalCardStyles = StyleSheet.create({
     infoContainer:{
         width: 0.9*windowWidth,
         height: 0.09*windowHeight,
-        backgroundColor:"rgba(217, 217, 217, 0.85)",
+        backgroundColor:"rgba(217, 217, 217, 0.65)",
         position:'absolute',
-        bottom:0,
+        bottom:0.09*windowHeight,
         alignItems:"center",
         justifyContent:"center"
     },
@@ -46,7 +45,7 @@ const LocalCardStyles = StyleSheet.create({
     feesContainer:{
         position:'absolute',
         left:5,
-        bottom: 0.035*windowHeight
+        bottom: 0.12*windowHeight
     },
     likesContainer:{
         flexDirection:"row",
@@ -58,12 +57,22 @@ const LocalCardStyles = StyleSheet.create({
         flexDirection:"row",
         alignItems:"center",
         justifyContent:"center",
-        width: 0.8*windowWidth,
-        height: 0.07*windowHeight,
+        width: 0.9*windowWidth,
+        height: 0.09*windowHeight,
         backgroundColor:"rgba(140, 87, 186, 0.25)",
         borderBottomLeftRadius:5,
         borderBottomRightRadius:5
     },
+    CategorySubcontainer:{
+        margin:10,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    iconLabel:{
+        fontSize:12,
+        color:"white",
+        fontWeight:"600",
+    }
    
 });
 export default LocalCardStyles;
