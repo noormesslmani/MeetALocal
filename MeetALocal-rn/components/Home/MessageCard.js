f51ee347e829023fc432130038ce9a679d24c256
noormesslmani <noor.messlmani@gmail.com>
2022-11-05 14:36:04 +0200

    Frontend: created message card and implemented get user by id api

--- MeetALocal-rn/components/Home/MessageCard.js
+++ MeetALocal-rn/components/Home/MessageCard.js
@@ -6,10 +6,41 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import image from '../../assets/profile.jpg'
 import MessageCardStyles from '../ComponentsStyles/MessageCardStyles';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
 const MessageCard=({navigation, user})=> {
-    console.log(user)
+
+    const [image, setImage]=useState(null)
+    const [name, setName]=useState(null)
+    const [type_id, setType_id]=useState(null)
+    useEffect(()=>{
+        getUser()
+    },[])
+    async function getUser(){
+    const token = await AsyncStorage.getItem('@token')
+    axios({
+        method: "get",
+        headers: { Authorization: `Bearer ${token}`},
+        url:`http://192.168.1.7:8000/api/v1.0.0/users/user/${user.user}`,
+    })
+    .then((response)=> {
+        console.log(response.data)
+        setName(response.data.data.name)
+        setImage(response.data.data.profile_picture)
+        setType_id(response.data.data.type_id)
+        return response;
+    })
+    .catch(function (error) {
+        console.warn(error)
+    });
+    }
   return (
-    <></>
+    <View>
+    <TouchableOpacity style={MessageCardStyles.messageContainer} onPress={()=>console.log('hi')}>
+        <Image source={image?{ uri:`http://192.168.1.7:8000/${image}`}: require('../../assets/blank-profile.webp')} style={MessageCardStyles.avatar}/>
+        <Text>{name}</Text>
+    </TouchableOpacity>
+    </View>
   )
 }
 export default MessageCard
