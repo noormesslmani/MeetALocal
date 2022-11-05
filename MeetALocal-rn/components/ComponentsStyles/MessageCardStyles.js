268b5c31d421228a27014f6d632089187e988cf9
noormesslmani <noor.messlmani@gmail.com>
2022-11-05 14:13:57 +0200

    Frontend:  created message card component and styling sheet

---
+++ MeetALocal-rn/components/ComponentsStyles/MessageCardStyles.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const MessageCardStyles = StyleSheet.create({
+    
+   
+});
+export default MessageCardStyles;
