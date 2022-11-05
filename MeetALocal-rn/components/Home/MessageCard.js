d19cd56509a5816a91ee843a7f6cd83c61c9f247
noormesslmani <noor.messlmani@gmail.com>
2022-11-05 14:13:57 +0200

    Frontend:  created message card component and styling sheet

---
+++ MeetALocal-rn/components/Home/MessageCard.js
@@ -0,0 +1,15 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { useState, useEffect, useContext } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import image from '../../assets/profile.jpg'
+import MessageCardStyles from '../ComponentsStyles/MessageCardStyles';
+const MessageCard=({navigation, user})=> {
+    console.log(user)
+  return (
+    <></>
+  )
+}
+export default MessageCard
