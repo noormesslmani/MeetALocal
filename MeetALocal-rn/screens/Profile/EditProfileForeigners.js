744a2847e64ed83e25d4f76da3365fb12d4be535
noormesslmani <noor.messlmani@gmail.com>
2022-11-05 22:11:11 +0200

    Frontend: added user's info in edit profile screen and some styling

--- MeetALocal-rn/screens/Profile/EditProfileForeigners.js
+++ MeetALocal-rn/screens/Profile/EditProfileForeigners.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, TextInput, ScrollView, Pressable} from 'react-native'
 import React from 'react'
 import HomeStyles from '../General/Styles/HomeStyles';
 import { UserContext } from '../../App'
@@ -9,22 +9,165 @@ import { AntDesign } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import UploadImage from '../../components/UploadImage';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import DropDownPicker from 'react-native-dropdown-picker';
 const EditForeignerProfile=({navigation})=> {
     const { user, setUser} = useContext(UserContext);
     const [uri, setUri]= useState(null)
     const [base64, setBase64]= useState(null)
     const [ext, setext]= useState(null)
-    const [modalVisible, setModalVisible]= useState(false)
-    const [editModal, setEditModal]= useState(false)
+    const [name, setName]= useState(user.name)
+    const [phone, setPhone]= useState(user.phone)
+    const [nationality, setNationality]=useState(user.nationality);
+    const [residence, setResidence]=useState(user.residence);
+    const [spokenLanguages, setSpokenLanguages]=useState(user.languages);
+    const [openNationality, setOpenNationality] = useState(false);
+    const [openResidece, setOpenResidence] = useState(false);
+    const [openLanguages, setOpenLanguages] = useState(false);
+    const [openGenders, setOpenGenders] = useState(false);
+    const [gender, setGender]= useState(user.gender)
+    const [genders, setGenders] = useState([
+        {label: 'Male', value: 'Male'},
+        {label: 'Female', value: 'Female'}])
+    const [countries, setCountries] = useState([
+        {label: 'Lebanon', value: 'Lebanon'},
+        {label: 'USA', value: 'USA'},
+        {label: 'Syria', value: 'Syria'},
+        {label: 'Egypt', value: 'Egypt'},
+        {label: 'KSA', value: 'KSA'},
+        {label: 'Turkey', value: 'Turkey'},
+        {label: 'France', value: 'France'},
+        {label: 'Iran', value: 'Iran'},
+        {label: 'Germany', value: 'Germany'},
+        {label: 'Brazil', value: 'Brazil'},
+        {label: 'Italy', value: 'Italy'},
+        {label: 'Jordan', value: 'Jordan'},
+        {label: 'Morocco', value: 'Morocco'},
+        {label: 'Canada', value: 'Canada'},
+      ]);
+      const [languages, setLanguages] = useState([
+        {label: 'English', value: 'English'},
+        {label: 'French', value: 'French'},
+        {label: 'Arabic', value: 'Arabic'},
+        {label: 'Spanish', value: 'Spanish'},
+        {label: 'Turkish', value: 'Turkish'},
+        {label: 'German', value: 'German'},
+        {label: 'Russian', value: 'Russian'},
+        {label: 'Persian', value: 'Persian'},
+      ]);
     useEffect(()=>{
       if(user.profile_picture){
         setUri(`http://192.168.1.7:8000/${user.profile_picture}`)
       }
     },[user.profile_picture])
-    console.log(uri)
+   
   return (
     <View style={ProfileStyles.container}>
         <UploadImage setBase64={setBase64} setext={setext} uri={uri} />
+        <KeyboardAwareScrollView contentContainerStyle={{paddingBottom:50}} showsVerticalScrollIndicator={false}>
+            <View style={ProfileStyles.inputContainer}>
+                <View style={{margin:10}}>
+                    <Text>Name</Text>
+                    <TextInput value={name} onChange={setName} style={ProfileStyles.input}/>
+                </View>
+                <View style={{margin:10}}>
+                    <Text>Phone Number</Text>
+                    <TextInput value={phone.toString()} onChange={setPhone} style={ProfileStyles.input} keyboardType='numeric'/>
+                </View>
+                <View style={{margin:10}}>
+                    <Text>Nationality</Text>
+                    <DropDownPicker
+                    open={openNationality}
+                    value={nationality}
+                    zIndex={2000}
+                    zIndexInverse={2000}
+                    dropDownDirection="TOP"
+                    items={countries}
+                    setOpen={setOpenNationality}
+                    setValue={setNationality}
+                    setItems={setCountries}
+                    dropDownContainerStyle={ProfileStyles.dropDownContainer}
+                    style={ProfileStyles.dropDown}
+                    placeholder="Select a country"
+                    placeholderStyle={{
+                    color: "grey"
+                    }}
+                    listMode="SCROLLVIEW"
+                    closeAfterSelecting={true}
+                    />
+                </View>
+                <View style={{margin:10}}>
+                    <Text>Country of Residence</Text>
+                    <DropDownPicker
+                    open={openResidece}
+                    value={residence}
+                    zIndex={3000}
+                    zIndexInverse={2000}
+                    dropDownDirection="TOP"
+                    items={countries}
+                    setOpen={setOpenResidence}
+                    setValue={setResidence}
+                    setItems={setCountries}
+                    dropDownContainerStyle={ProfileStyles.dropDownContainer}
+                    style={ProfileStyles.dropDown}
+                    placeholder="Select a country"
+                    placeholderStyle={{
+                    color: "grey"
+                    }}
+                    listMode="SCROLLVIEW"
+                    closeAfterSelecting={true}
+                    />
+                </View>
+                <View style={{margin:10}}>
+                    <Text>Languages</Text>
+                    <DropDownPicker
+                    open={openLanguages}
+                    multiple={true}
+                    mode="BADGE"
+                    value={spokenLanguages}
+                    zIndex={1000}
+                    zIndexInverse={3000}
+                    dropDownDirection="TOP"
+                    items={languages}
+                    setOpen={setOpenLanguages}
+                    setValue={setSpokenLanguages}
+                    setItems={setLanguages}
+                    dropDownContainerStyle={ProfileStyles.dropDownContainer}
+                    style={ProfileStyles.dropDown}
+                    placeholder="Select languages"
+                    listMode="SCROLLVIEW"
+                    placeholderStyle={{
+                    color: "grey"
+                    }}
+                    />
+                </View>
+                <View style={{margin:10}}>
+                    <Text>Gender</Text>
+                    <DropDownPicker
+                    open={openGenders}
+                    value={gender}
+                    zIndex={1000}
+                    zIndexInverse={3000}
+                    dropDownDirection="TOP"
+                    items={genders}
+                    setOpen={setOpenGenders}
+                    setValue={setGender}
+                    setItems={setGenders}
+                    dropDownContainerStyle={ProfileStyles.dropDownContainer}
+                    style={ProfileStyles.dropDown}
+                    placeholder="Select gender"
+                    listMode="SCROLLVIEW"
+                    placeholderStyle={{
+                    color: "grey"
+                    }}
+                    />
+                </View>
+                <View style={ProfileStyles.btnContainer}>
+                    <Pressable style={ProfileStyles.btn}><Text>Save</Text></Pressable>
+                    <Pressable style={ProfileStyles.btn}><Text>Cancel</Text></Pressable>
+                </View>
+            </View>
+        </KeyboardAwareScrollView>
     </View>
     
   )
--- MeetALocal-rn/screens/Profile/ProfileStyles/ProfileStyles.js
+++ MeetALocal-rn/screens/Profile/ProfileStyles/ProfileStyles.js
@@ -5,7 +5,10 @@ const windowHeight = Dimensions.get("window").height;
 const ProfileStyles = StyleSheet.create({
     container:{
         alignItems:"center",
-        padding:20
+        padding:20,
+        width:windowWidth,
+        height:windowHeight,
+        backgroundColor:'white'
     },
     imgContainer:{
         elevation:2,
@@ -27,6 +30,54 @@ const ProfileStyles = StyleSheet.create({
         width: 0.9*windowWidth,
         margin:5,
     },
+    inputContainer:{
+        marginTop:30,
+        width:0.8*windowWidth,
+        alignItems:"center"
+    },
+    input:{
+        color:"grey",
+        width: windowWidth * 0.8,
+        height: 40,
+        marginTop: 5,
+        borderColor: "#4BB0F9",
+        borderBottomWidth: 0.7,
+    },
+    dropDown:{
+        marginTop:10,
+        borderTopWidth:0,
+        borderLeftWidth:0,
+        borderRightWidth:0,
+        borderBottomRightRadius:0,
+        borderBottomLeftRadius:0,
+        borderBottomColor:"#4BB0F9",
+        borderBottomWidth:0.5,
+        width:windowWidth * 0.8,
+    },
+    dropDownContainer:{
+        width:windowWidth * 0.8,
+        borderRadius:0,
+        borderWidth:0.5,
+    },
+    btnContainer:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        width:"60%"
+    },
+    btn:{
+        borderRadius: 20,
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop:40,
+        backgroundColor:"rgba(75, 176, 249, 0.3)",
+        marginBottom:10,
+        width:"45%",
+        alignItems:"center",
+        justifyContent:"center"
+    }
+    
 });
 
 export default ProfileStyles;
