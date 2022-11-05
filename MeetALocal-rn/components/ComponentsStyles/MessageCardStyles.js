066fb8a83224e89ff9921d95235d6771cb1a4b11
noormesslmani <noor.messlmani@gmail.com>
2022-11-05 14:36:04 +0200

    Frontend: created message card and implemented get user by id api

--- MeetALocal-rn/components/ComponentsStyles/MessageCardStyles.js
+++ MeetALocal-rn/components/ComponentsStyles/MessageCardStyles.js
@@ -3,7 +3,18 @@ import { StyleSheet, Dimensions } from "react-native";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 const MessageCardStyles = StyleSheet.create({
-    
+    messageContainer:{
+        width:windowWidth,
+        height:200,
+        flexDirection:"row",
+        backgroundColor:"white",
+        padding:20
+    },
+    avatar:{
+        width:100,
+        height:100,
+        borderRadius:50
+    }
    
 });
 export default MessageCardStyles;
