cdfceab2b6a60052ac32500a6dbe616fdfa22793
noormesslmani <noor.messlmani@gmail.com>
2022-11-05 18:20:39 +0200

    Frontend: implemented message sending functionality and displayed chat

--- MeetALocal-rn/screens/General/ChatScreen.js
+++ MeetALocal-rn/screens/General/ChatScreen.js
@@ -22,30 +22,57 @@ const ChatScreen=()=> {
     const [messages, setMessages] = useState([]);
     const { user, setUser} = useContext(UserContext);
     const uri=`http://192.168.1.7:8000/${user.profile_picture}`
-  useEffect(() => {
-    getMessages()
-  }, []);
-  console.log(chatId)
-  const getMessages = async () => {
-    const docRef = doc(database, "chats", chatId);
-    const colRef = collection(docRef, "messages");
-    const q = query(colRef, orderBy("createdAt", "desc"));
-    const unsubscribe = onSnapshot(q, (snapshot) =>
-        setMessages(
-            snapshot.docs.map((doc) => ({
-                createdAt: doc.data().createdAt.toDate(),
-                text: doc.data().text,
-                user: doc.data().user,
-            }))
-        )
-    );
-    return () => {
-        unsubscribe();
+    useEffect(() => {
+        getMessages()
+    }, []);
+    console.log(chatId)
+    const getMessages = async () => {
+        const docRef = doc(database, "chats", chatId);
+        const colRef = collection(docRef, "messages");
+        const q = query(colRef, orderBy("createdAt", "desc"));
+        const unsubscribe = onSnapshot(q, (snapshot) =>
+            setMessages(
+                snapshot.docs.map((doc) => ({
+                    _id: doc.data()._id,
+                    createdAt: doc.data().createdAt.toDate(),
+                    text: doc.data().text,
+                    user: doc.data().user,
+                }))
+            )
+        );
+        return () => {
+            unsubscribe();
+        };
     };
-};
-console.log(messages)
-  return (
-    <></>
-  )
+    console.log(messages)
+
+    const onSend = useCallback((messages = []) => {
+        setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, messages)
+        );
+        const { _id, createdAt, text, user } = messages[0];
+        addDoc(collection(database, `chats/${chatId}`, "messages"), {
+            _id,
+            createdAt,
+            text,
+            user,
+        });
+    }, []);
+    return (
+        <GiftedChat
+            showUserAvatar={true}
+            isTyping={true}
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: user.id,
+                name: user.name,
+                avatar:uri,
+            }}
+            alwaysShowSend
+            scrollToBottom
+            renderUsernameOnMessage={true}
+        />
+    )
 }
 export default ChatScreen
