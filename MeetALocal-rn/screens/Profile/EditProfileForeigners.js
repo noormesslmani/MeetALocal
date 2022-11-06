0ce7b98ce721962a28728e8bd32566b660b67b6e
noormesslmani <noor.messlmani@gmail.com>
2022-11-06 13:30:01 +0200

    Frontend: added date of birth to profile edit

--- MeetALocal-rn/screens/Profile/EditProfileForeigners.js
+++ MeetALocal-rn/screens/Profile/EditProfileForeigners.js
@@ -4,7 +4,7 @@ import HomeStyles from '../General/Styles/HomeStyles';
 import { UserContext } from '../../App'
 import { useState, useEffect, useContext } from "react";
 import ProfileStyles from './ProfileStyles/ProfileStyles';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/AntDesign'
 import { AntDesign } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -13,6 +13,7 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import DropDownPicker from 'react-native-dropdown-picker';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from 'axios';
+import DateTimePicker from '@react-native-community/datetimepicker';
 const EditForeignerProfile=({navigation})=> {
     const { user, setUser} = useContext(UserContext);
     const [uri, setUri]= useState(null)
@@ -27,6 +28,9 @@ const EditForeignerProfile=({navigation})=> {
     const [openResidece, setOpenResidence] = useState(false);
     const [openLanguages, setOpenLanguages] = useState(false);
     const [openGenders, setOpenGenders] = useState(false);
+    const [datePicker, setDatePicker]= useState(false)
+    const [date, setDate]= useState(new Date())
+    const [dob, setdob]= useState(user.date_of_birth)
     const [gender, setGender]= useState(user.gender)
     const [about, setAbout]= useState(user.about)
     const [genders, setGenders] = useState([
@@ -58,6 +62,12 @@ const EditForeignerProfile=({navigation})=> {
         {label: 'Russian', value: 'Russian'},
         {label: 'Persian', value: 'Persian'},
       ]);
+      console.log(user.date_of_birth)
+      const handleDate= (event, value)=>{
+        setDatePicker(false)
+        setDate(value)
+        setdob(`${value.getFullYear()}-${value.getMonth()+1}-${value.getDate()}`)
+      }
     useEffect(()=>{
       if(user.profile_picture){
         setUri(`http://192.168.1.7:8000/${user.profile_picture}`)
@@ -73,7 +83,8 @@ const EditForeignerProfile=({navigation})=> {
             about,
             photo:base64,
             ext,
-            languages:spokenLanguages
+            languages:spokenLanguages,
+            date_of_birth: dob
           };
           const token = await AsyncStorage.getItem('@token')
           axios({
@@ -83,7 +94,6 @@ const EditForeignerProfile=({navigation})=> {
             url:"http://192.168.1.7:8000/api/v1.0.0/users/edit-profile",
           })
           .then(async (response)=> {
-            console.log(response.data.data)
             setUser(response.data.data)
             return response.data;
           })
@@ -108,6 +118,18 @@ const EditForeignerProfile=({navigation})=> {
                     <Text>About</Text>
                     <TextInput placeholder='Write something about yourself' multiline={true} value={about} onChangeText={setAbout} style={ProfileStyles.input} maxLength={200}/>
                 </View>
+                <View style={{margin:10}}>
+                    <Text>Date of birth</Text>
+                    <TouchableOpacity onPress={()=>setDatePicker(true)} style={{alignSelf:'center', marginTop:10}}><Icon name="calendar" size={25}/></TouchableOpacity>
+                </View>
+                { datePicker && <DateTimePicker
+                  value={date}
+                  mode={'date'}
+                  is24Hour={true}
+                  onChange={handleDate}
+                  maximumDate={new Date(2022, 1, 1)}
+                  minimumDate={new Date(1900, 0, 1)}
+                  />}
                 <View style={{margin:10}}>
                     <Text>Nationality</Text>
                     <DropDownPicker
