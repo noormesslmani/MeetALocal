68886fd67eabd8d152589046a0d4f48722dddc86
noormesslmani <noor.messlmani@gmail.com>
2022-11-07 00:40:39 +0200

    Frontend: created constants folder and added category icons object

---
+++ MeetALocal-rn/constants/categories.js
@@ -0,0 +1,11 @@
+import Tourism from '../assets/tourism.png'
+import Culture from '../assets/cultures.png'
+import Education from '../assets/education.png'
+import Guidance from '../assets/guidance.png'
+import History from '../assets/history.png'
+import Housing from '../assets/house.png'
+import Language from '../assets/languages.png'
+import Other from '../assets/more.png'
+import Jobs from '../assets/suitcase.png'
+export const categoryIcons={'Tourism':Tourism, 'Culture':Culture, 'Education':Education, 'Guidance': Guidance,
+'History': History, "Housing":Housing, 'Language':Language, 'Other':Other, 'Jobs':Jobs}
