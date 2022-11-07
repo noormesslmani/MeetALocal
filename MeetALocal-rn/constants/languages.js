ecbf25225015e3c82990f0977896ef745bcb2300
noormesslmani <noor.messlmani@gmail.com>
2022-11-07 14:14:00 +0200

    Frontend: created langauge options array

---
+++ MeetALocal-rn/constants/languages.js
@@ -0,0 +1,10 @@
+export const languagesOptions=[
+    {label: 'English', value: 'English'},
+    {label: 'French', value: 'French'},
+    {label: 'Arabic', value: 'Arabic'},
+    {label: 'Spanish', value: 'Spanish'},
+    {label: 'Turkish', value: 'Turkish'},
+    {label: 'German', value: 'German'},
+    {label: 'Russian', value: 'Russian'},
+    {label: 'Persian', value: 'Persian'},
+  ]
