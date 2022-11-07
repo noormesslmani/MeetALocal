35fd9d968ff0e879282ed743fbf1cfffa33c83cd
noormesslmani <noor.messlmani@gmail.com>
2022-11-07 21:00:13 +0200

    Frontend: removed some uneccessary code

--- MeetALocal-rn/components/Cards/HomeCard.js
+++ MeetALocal-rn/components/Cards/HomeCard.js
@@ -1,8 +1,6 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { useState, useEffect, useContext } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeStyles from '../../screens/General/Styles/HomeStyles';
 import Icon from 'react-native-vector-icons/FontAwesome5'
 const HomeCard=({label, handlePress})=> {
