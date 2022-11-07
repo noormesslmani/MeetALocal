800483440866c45b2baccedf6e5548a3b4a5485a
noormesslmani <noor.messlmani@gmail.com>
2022-11-07 14:05:30 +0200

    Frontend: created categories options array

--- MeetALocal-rn/constants/categories.js
+++ MeetALocal-rn/constants/categories.js
@@ -7,5 +7,18 @@ import Housing from '../assets/house.png'
 import Language from '../assets/languages.png'
 import Other from '../assets/more.png'
 import Jobs from '../assets/suitcase.png'
+import { Image } from 'react-native'
 export const categoryIcons={'Tourism':Tourism, 'Culture':Culture, 'Education':Education, 'Guidance': Guidance,
 'History': History, "Housing":Housing, 'Language':Language, 'Other':Other, 'Jobs':Jobs}
+export const categoriesOptions=[
+    {label: 'All categories', value: 'all'},
+    {label: 'Tourism', value: 'Tourism', icon: () => <Image source={Tourism} style={{width:20, height:20}} />},
+    {label: 'Languages', value: 'Languages', icon: () => <Image source={Language} style={{width:20, height:20}}/>},
+    {label: 'Culture', value: 'Culture', icon: () => <Image source={Culture} style={{width:20, height:20}}/>},
+    {label: 'Education', value: 'Education', icon: () => <Image source={Education} style={{width:20, height:20}}/>},
+    {label: 'History', value: 'History', icon: () => <Image source={History} style={{width:20, height:20}}/>},
+    {label: 'Guidance', value: 'Guidance', icon: () => <Image source={Guidance} style={{width:20, height:20}}/>},
+    {label: 'Jobs', value: 'Jobs', icon: () => <Image source={Jobs} style={{width:20, height:20}}/>},
+    {label: 'Housing', value: 'Housing', icon: () => <Image source={Housing} style={{width:20, height:20}}/>},
+    {label: 'Other', value: 'Other', icon: () => <Image source={Other} style={{width:20, height:20}}/>},
+    ]
