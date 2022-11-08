757e095635fd14e3520c916f75f841275a2e46ae
noormesslmani <noor.messlmani@gmail.com>
2022-11-08 22:43:09 +0200

    Frontend: created a reusable app button

---
+++ MeetALocal-rn/components/Buttons/AppButtons.js
@@ -0,0 +1,27 @@
+import { View, Text, TouchableOpacity, Image, FlatList, SafeAreaView, Modal, Pressable, StyleSheet, ScrollView, TextInput, KeyboardAvoidingView} from 'react-native'
+import React from 'react'
+import { widths } from '../../constants/dimensions'
+import { colors } from '../../constants/colors'
+const AppButton=({text, handlePress})=>{
+    return (
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.text}>{text}</Text>
+        </TouchableOpacity>
+    )
+}
+export default AppButton
+const styles=StyleSheet.create({
+    button:{
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor:colors.lightBlue,
+        borderRadius: 20,
+        padding:10,
+        width:0.25*widths.width,
+        height:40
+    },
+    text:{
+        fontWeight:"700",
+        color:"white"
+    }
+})
