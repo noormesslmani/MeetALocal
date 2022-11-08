572130f1a2ccbe6cdbf26ab1687431afcfe2b729
noormesslmani <noor.messlmani@gmail.com>
2022-11-08 21:41:06 +0200

    Frontend: refactored datet picker into reusable component

--- MeetALocal-rn/components/General/datePicker.js
+++ MeetALocal-rn/components/General/datePicker.js
@@ -1,14 +1,26 @@
 import DateTimePicker from '@react-native-community/datetimepicker';
-const DatePicker=({date, handleDate})=>{
+const DatePicker=({date, handleDate, type=1})=>{
     return(
-        <DateTimePicker
+        <>
+        {type==1 &&<DateTimePicker
         value={date}
         mode={'date'}
         is24Hour={true}
         onChange={handleDate}
         maximumDate={new Date(2022, 1, 1)}
         minimumDate={new Date(1900, 0, 1)}
-        />
+        />}
+        {
+            type==2 &&
+            <DateTimePicker
+            value={date}
+            mode={'date'}
+            is24Hour={true}
+            onChange={handleDate}
+            minimumDate={new Date()}
+            />
+        }
+        </>
     )
 }
 export default DatePicker
