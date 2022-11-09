ed7dc2cc84291a29f43348f09a0717299dbacd73
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 00:29:14 +0200

    Frontend:created login page

---
+++ admin-panel/src/Pages/LogIn/LogIn.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import { useNavigate } from "react-router-dom";
+import Header from '../../Components/Header/Header';
+import LogInForm from '../../Components/Forms/LogInForm';
+import './LogIn.css'
+const LogIn=()=> {
+  const navigate = useNavigate();
+  return (
+    <div className='container flex-col'>
+      <Header/>
+      <div className='body'>
+        <LogInForm />
+      </div>
+  
+    </div>
+  )
+}
+export default LogIn
