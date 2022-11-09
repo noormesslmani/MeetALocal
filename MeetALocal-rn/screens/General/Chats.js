c63e9208e5d82000c13b72366ef94720099f47ce
noormesslmani <noor.messlmani@gmail.com>
2022-11-09 16:11:26 +0200

    Frontend: stored chat details in messages

--- MeetALocal-rn/screens/General/Chats.js
+++ MeetALocal-rn/screens/General/Chats.js
@@ -14,32 +14,38 @@ import {
   getDocs,
   limit
 } from "firebase/firestore";
+import { address } from '../../constants/address';
 const Chats=({navigation})=> {
   const [chats, setChats]= useState([])
+  const [messages, setMessages]= useState([])
   const { user, setUser} = useContext(UserContext);
- 
-  const uri=`http://192.168.1.7:8000/${user.profile_picture}`
+  const uri=`${address}/${user.profile_picture}`
 
   useEffect(()=>{
-    setChats([])
     getChats()
   },[])
  
   async function getChats(){
+  //query chats
+  setChats([])
   const q = query(collection(database, "chats"), where("users", "array-contains", user.id));
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach(async (doc) => {
+    //query last message
+  
     const q = query(collection(database, `chats/${doc.id}/messages`), orderBy("createdAt", "desc"), limit(1))
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach(async (doc2) => {
-        setChats((chats)=>[...chats,{ chat_id: doc.id, user_id:doc.data().users.filter(id=>id!=user.id)[0], date:doc2.data().createdAt.toDate(), text: doc2.data().text }])
+      setChats((chats)=>[...chats,{ chat_id: doc.id, user_id:doc.data().users.filter(id=>id!=user.id)[0], date:doc2.data().createdAt.toDate(), text: doc2.data().text }])
       })
-
     })
   }
+  useEffect(()=>{
+    setMessages(chats)
+  },[chats])
   return (
             <ScrollView>
-              {chats.map((chat)=><MessageCard chat={chat} navigation={navigation} key={chat.chat_id}/>)}
+              {messages.map((chat)=><MessageCard chat={chat} navigation={navigation} key={chat.chat_id}/>)}
             </ScrollView>
           
   )
