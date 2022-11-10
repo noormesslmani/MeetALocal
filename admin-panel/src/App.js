53889d51addae45a83127ece6adf460c62332ef2
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 22:50:46 +0200

    Frontend: added some routes

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogIn from './Pages/LogIn/LogIn';
 import Home from './Pages/Home/Home';
+import Locals from './Pages/Users/Locals';
+import Foreigners from './Pages/Users/Foreigners';
+import LocalStatistics from './Pages/Statistics/LocalStatistics';
 function App() {
   return (
     <>
@@ -9,6 +12,9 @@ function App() {
         <Routes>
           <Route path='/' exact element={<LogIn/>} />
           <Route path='/home' exact element={<Home/>} />
+          <Route path='/locals' exact element={<Locals/>} />
+          <Route path='/foreigners' exact element={<Foreigners/>} />
+          <Route path='/locals-statistics' exact element={<LocalStatistics/>} />
         </Routes>
       </Router>
     </>
