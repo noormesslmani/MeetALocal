cac35816e1321bc45b4772bdad0ae94335b64e9f
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 02:04:20 +0200

    Frontend:changed the props ofheader and logo

--- admin-panel/src/Components/Logo/Logo.js
+++ admin-panel/src/Components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from '../../Assets/logo.png'
 import  './Logo.css'
-const Logo=({type})=>{
+const Logo=()=>{
     return(
-        <img src={logo} className={type}></img>
+        <img src={logo} className='logo1'></img>
     )
 }
 export default Logo
