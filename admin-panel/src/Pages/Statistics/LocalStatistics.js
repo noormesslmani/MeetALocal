d0c836bb406600a4cf4ba6b7b9d55c7f3d9ae2a1
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 22:20:31 +0200

    Frontend: displayed some statistics for locals in pie charts

--- admin-panel/src/Pages/Statistics/LocalStatistics.js
+++ admin-panel/src/Pages/Statistics/LocalStatistics.js
@@ -6,52 +6,45 @@ import Header from '../../Components/Header/Header';
 import NavBar from '../../Components/NavBar/NavBar';
 import CanvasJSReact from '../../canvasjs.react';
 import { getLocalsStat } from '../../Network/Api';
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+import { Bounce } from "react-activity";
+import "react-activity/dist/library.css";
+import PieChart from '../../Components/Chats/PieCharts';
 const LocalStatistics=({type})=> {
-  const [genderData, setGenderData]= useState({})
-  const [ageData, setAgeData]= useState({})
+  const [total, setTotal]=useState(null)
+  const [genderData, setGenderData]= useState(null)
+  const [ageData, setAgeData]= useState(null)
   const [isLoading, setIsLoading]= useState(false)
+  const [topCategories, setTopCategories]=useState([])
   useEffect(()=>{
     getStat()
   },[])
-  // const options = {
-  //   animationEnabled: true,
-  //   title: {
-  //     text: "Website Traffic Sources"
-  //   },
-  //   data: [{
-  //     type: "pie",
-  //     startAngle: 0,
-  //     showInLegend: "true",
-  //     legendText: "{label}",
-  //     toolTipContent: "{label}: <strong>{y}%</strong>",
-  //     indexLabel: "{y}%",
-  //     indexLabelPlacement: "inside",
-  //     dataPoints: [
-  //       { y: 18, label: "Direct" },
-  //       { y: 49, label: "Organic Search" },
-  //       { y: 9, label: "Paid Search" },
-  //       { y: 5, label: "Referral" },
-  //       { y: 19, label: "Social" }
-  //     ]
-  //   }]
-  // }
+ 
+  console.log(genderData)
   const getStat= async()=>{
     setIsLoading(true)
     const result =await getLocalsStat()
     if (result.success){
         console.log(result.data.data)
-    }
+        setTotal(result.data.data.locals_nb)
+        setGenderData([Math.round(result.data.data['male%']),Math.round(result.data.data['female%'])])
+        setAgeData([Math.round(result.data.data.ages[0]),Math.round(result.data.data.ages[1]),Math.round(result.data.data.ages[2])])
+      }
     setIsLoading(false)
 }
+
   return(
     <div className='home-container'>
         <Header type={2}/>
         <div className='flex'>
             <NavBar/>
             <div className='dashboard-container flex-col align-center'>
-            {/* <CanvasJSChart options = {options}/> */}
+            {isLoading && <Bounce color='rgba(140,87,186,0.7)'/>}
+            <div className='flex'>
+            {!isLoading && <PieChart
+              options={{ labels: ["Male", "Female"] }} series={genderData} title={'Genders'}/>}
+              {!isLoading && <PieChart
+              options={{ labels: ["Below 30", "30-60","Above 60"] }} series={ageData} title={'Age Groups'} />}
+            </div>
             </div>
         </div>
     </div>
