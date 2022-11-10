3338352849eee60f901e797bcb16711fca3010f2
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 02:53:27 +0200

    Frontend: created count card components to display stats

--- admin-panel/src/Pages/Home/Home.js
+++ admin-panel/src/Pages/Home/Home.js
@@ -5,9 +5,12 @@ import Header from '../../Components/Header/Header';
 import NavBar from '../../Components/NavBar/NavBar';
 import './Home.css'
 import { getAppStat } from '../../Network/Api';
-
+import CountsCard from '../../Components/Cards/CountsCard';
 const Home=()=> {
-    const [counts, setCounts]=useState([])
+    const [usersCount, setUsersCount]=useState(null)
+    const [eventCount, setEventsCount]=useState(null)
+    const [postsCount, setPostsCount]=useState(null)
+    const [commentsCount, setCommentsCount]=useState(null)
     useEffect(()=>{
         appStat()
     },[])
@@ -15,21 +18,30 @@ const Home=()=> {
     const appStat= async()=>{
         const result =await getAppStat()
         if (result.success){
-            setCounts([result.data.data.users_nb,result.data.data.events_nb, result.data.data.posts_nb, result.data.data.comments_nb])
+            setUsersCount(result.data.data.users_nb)
+            setEventsCount(result.data.data.events_nb)
+            setPostsCount(result.data.data.posts_nb)
+            setCommentsCount(result.data.data.comments_nb)
         }
     }
-    console.log(counts)
   return (
-    <>
+    <div className='home-container'>
         <Header type={2}/>
         <div className='flex'>
             <NavBar/>
             <div className='dashboard-container flex-col align-center'>
-                <h2>Welcome</h2>
-                {counts.map((count, index)=><p>{count} {index}</p>)}
+                <h1 className='welcome'>General Statistics</h1>
+                <div className='flex wrap'>
+                    <CountsCard count={usersCount} index={0}/>
+                    <CountsCard count={eventCount} index={1}/>
+                </div>
+                <div className='flex wrap'>
+                    <CountsCard count={postsCount} index={2}/>
+                    <CountsCard count={commentsCount} index={3}/>
+                </div>
             </div>
         </div>
-    </>
+    </div>
   )
 }
 export default Home
