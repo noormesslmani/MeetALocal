1866a68395a73cf63809e8be1da0561253588c61
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 22:19:34 +0200

    Frontend: created a pie chart component

---
+++ admin-panel/src/Components/Chats/PieCharts.js
@@ -0,0 +1,10 @@
+import React, {useState, Component, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
+
+import Chart from 'react-apexcharts'
+const PieChart=({options, series, title})=>{
+    return(
+        <Chart  options={options} series={series} type="pie" width="360" />
+    )
+}
+export default PieChart
