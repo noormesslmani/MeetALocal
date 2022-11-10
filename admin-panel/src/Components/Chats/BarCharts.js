79ec286c443da1b6ae1872cc9c8892ec2de9647d
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 22:48:51 +0200

    Frontend: created bar chart component

---
+++ admin-panel/src/Components/Chats/BarCharts.js
@@ -0,0 +1,11 @@
+import React, {useState, Component, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
+
+import Chart from 'react-apexcharts'
+const BarChart=({options, series, title})=>{
+    console.log(series)
+    return(
+        <Chart  options={options} series={series} type="bar" width="360" />
+    )
+}
+export default BarChart
