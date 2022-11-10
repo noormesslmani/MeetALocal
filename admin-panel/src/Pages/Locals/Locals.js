3a1cad1a19c1a80053e63342c9b856c1680c1242
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 15:48:05 +0200

    Frontend: queried locals according to page number

--- admin-panel/src/Pages/Locals/Locals.js
+++ admin-panel/src/Pages/Locals/Locals.js
@@ -9,10 +9,14 @@ import { Bounce } from "react-activity";
 import "react-activity/dist/library.css";
 import UsersTable from '../../Components/UsersTable/UserTable';
 import {getUsers} from '../../Network/Api'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft} from '@fortawesome/free-solid-svg-icons'
 const Locals=()=> {
     const [isLoading, setIsLoading]= useState(true)
     const [data, setData]=useState(null)
     const [page, setPage]=useState(1)
+    const [currentPage, setCurrentPage]=useState(1)
     useEffect(()=>{
         getLocals()
     },[page])
@@ -22,9 +26,22 @@ const Locals=()=> {
         const result =await getUsers('Local', 10*(page-1))
         if (result.success){
             console.log(result.data.data)
+            if(result.data.data.length==0){
+                setPage(page-1)
+            }
+            else{
             setData(result.data.data)
+            setCurrentPage(page)}
         }
         setIsLoading(false)
+    }
+    const hanldeNext=()=>{
+        setPage(page+1)
+    }
+    const hanldePrev=()=>{
+        if(page>1){
+            setPage(page-1)
+        }
     }
   return (
     <div className='home-container'>
@@ -33,6 +50,11 @@ const Locals=()=> {
             <NavBar/>
             <div className='dashboard-container flex-col align-center'>
                 <h1 className='home-title'>Locals</h1>
+                <div className='flex align-center justify-center'>
+                    <FontAwesomeIcon icon={faArrowLeft} color='rgba(140,87,186,1)' className='arrow' onClick={hanldePrev}/>
+                    <p>{currentPage}</p>
+                    <FontAwesomeIcon icon={faArrowRight} color='rgba(140,87,186,1)' className='arrow' onClick={hanldeNext}/>
+                    </div>
                 {isLoading && <Bounce color='rgba(140,87,186,0.7)'/>}
                 {!isLoading && <UsersTable data={data}/>}
             </div>
