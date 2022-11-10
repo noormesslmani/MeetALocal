a2655a948e8eb9d845771116f859aa76bc672101
noormesslmani <noor.messlmani@gmail.com>
2022-11-10 02:03:09 +0200

    Frontend: created a variation of header component

--- admin-panel/src/Components/Header/Header.js
+++ admin-panel/src/Components/Header/Header.js
@@ -2,12 +2,28 @@ import React, {useState} from 'react';
 import { useNavigate } from "react-router-dom";
 import Logo from '../Logo/Logo';
 import '../../Constants/Flex.css'
-const Header=()=> {
+import profile from '../../Assets/blank-profile.webp'
+import './Header.css'
+const Header=({type})=> {
+  const  user=JSON.parse(localStorage.getItem('user'))
   const navigate = useNavigate();
-  return (
-    <header className='flex justify-center align-center header'>
-        <Logo type={'logo1'} class/>
-    </header>
-  )
+  if(type==1){
+    return (
+      <header className='flex justify-center align-center header'>
+          <Logo/>
+      </header>
+    )
+  }
+  if(type==2){
+    return (
+      <header className='flex align-center header space-between'>
+          <Logo/>
+          <div className='flex align-center justify-center'>
+            <p>{user.name}</p>
+            <img src={ user.profile_picture? `http://127.0.0.1:8000/${user.profile_picture}`:{profile}} className='profile-img'></img>
+          </div>
+      </header>
+    )
+  }
 }
 export default Header
