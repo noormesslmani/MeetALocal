b5b104694d6a3b8dab5ef82d64173533f082ac1b
noormesslmani <noor.messlmani@gmail.com>
2022-11-11 22:53:23 +0200

    Frontend: moved the flat list to the map card component

--- MeetALocal-rn/components/Cards/MapCard.js
+++ MeetALocal-rn/components/Cards/MapCard.js
@@ -1,26 +1,42 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
 import React from 'react'
 import { useState, useEffect, useContext } from "react";
 import { address } from '../../constants/address';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { colors } from '../../constants/colors';
 import MapCardStyles from '../ComponentsStyles/MapCardStyles';
-const MapCard=({item})=> {
-    return(
-        <TouchableOpacity >
-          <View style={MapCardStyles.card_view} >
-            <Image
-              source={item.profile_picture?{ uri:`http://192.168.1.7:8000/${item.profile_picture}`}: require('../../assets/blank-profile.webp')}
-              style={MapCardStyles.card_image}
-            />
-            <View style={{flex:2,padding:10}}>
-              <Text numberOfLines={1} style={MapCardStyles.title}>{item.name}</Text>
-              <Text numberOfLines={1} style={MapCardStyles.country}>{item.country}</Text>
-              <View style={MapCardStyles.card_inner_view}>
+const MapCard=({data, myList})=> {
+    const renderItem = ({ item, index }) => (
+        <TouchableOpacity 
+            >
+              <View style={MapCardStyles.card_view} >
+                <Image
+                  source={item.profile_picture?{ uri:`http://192.168.1.7:8000/${item.profile_picture}`}: require('../../assets/blank-profile.webp')}
+              
+                  style={MapCardStyles.card_image}
+                />
+                <View style={{flex:2,padding:10}}>
+                  <Text numberOfLines={1} style={MapCardStyles.title}>{item.name}</Text>
+                  <Text numberOfLines={1} style={MapCardStyles.country}>{item.country}</Text>
+                  <View style={MapCardStyles.card_inner_view}>
+                  </View>
+                </View>
               </View>
-            </View>
-          </View>
-      </TouchableOpacity>
+          </TouchableOpacity>
+      );
+    return(
+        <FlatList
+            horizontal
+            keyExtractor={item => item.id.toString()}
+            scrollEventThrottle={1}
+            showsHorizontalScrollIndicator={false}
+            style={MapCardStyles.card_scroll_view}
+            data={data}
+            ref={myList}
+            initialScrollIndex={0}
+            renderItem={renderItem}
+        >
+        </FlatList>
     )
 }
 export default MapCard
--- MeetALocal-rn/components/map/Map.js
+++ MeetALocal-rn/components/map/Map.js
@@ -2,22 +2,21 @@ import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image, FlatList} 
 import React, { useState, useRef, useEffect } from 'react'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import {Marker, Callout} from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { colors } from '../../constants/colors';
+import Animated from "react-native-reanimated";
 import MapCard from '../Cards/MapCard';
 const Map=({lat, lng, data, type, handleDrag, navigation})=> { 
   const myList = useRef()
-  const renderItem = ({ item, index }) => (
-    <MapCard item={item}/>
-  );
   
   return (
               <>
                 <MapView
                   region= {{
-                    latitude: lat,
-                    longitude: lng,
-                    latitudeDelta: 5,
-                    longitudeDelta: 5,
+                      latitude: lat,
+                      longitude: lng,
+                      latitudeDelta: 5,
+                      longitudeDelta: 5,
                   }}
                   style={[type ==1 && styles.map, type==2 && styles.map2]}
                   loadingEnabled={true}
@@ -25,16 +24,14 @@ const Map=({lat, lng, data, type, handleDrag, navigation})=> {
                   provider={PROVIDER_GOOGLE}
                   maxZoomLevel={15}>
 
-                {/* type 1 Markers */}
                   {type==1 && data.map((local, index) => (
                   <Marker
                   coordinate={{latitude: local.latitude, longitude: local.longitude}}
                   pinColor={colors.violet}
                   onPress={()=> myList.current.scrollToIndex({index: index, animated: true })}
                   >
-                  </Marker>))}  
+                  </Marker>))} 
 
-                {/* type 2 Markers */}
                   {type==2 && 
                   <Marker coordinate={{latitude: lat,
                   longitude:lng}}
@@ -43,20 +40,7 @@ const Map=({lat, lng, data, type, handleDrag, navigation})=> {
                   onDragEnd={handleDrag}>
                   </Marker>} 
                 </MapView>
-
-                {/* list of cards for type 1 map */}
-                {type==1 && 
-                  <FlatList
-                  horizontal
-                  keyExtractor={item => item.id.toString()}
-                  scrollEventThrottle={1}
-                  showsHorizontalScrollIndicator={false}
-                  style={styles.card_scroll_view}
-                  data={data}
-                  ref={myList}
-                  initialScrollIndex={0}
-                  renderItem={renderItem}>
-                </FlatList>}
+                {type==1 && <MapCard data={data} myList={myList}/> }
               </>
   )
 }
@@ -72,7 +56,36 @@ const styles = StyleSheet.create({
     width:"100%",
     height:"85%",
     },
-    
+    card_scroll_view: {
+      position:'absolute',
+      bottom:10,
+      left:10,
+      right:0,
+      paddingVertical:8
+      },
+      card_view: {
+      elevation:90,
+      backgroundColor:'#fff',
+      borderRadius:10,
+      marginHorizontal: 10,
+      height: 200,
+      width: 250,
+      overflow: 'hidden'
+      },
+      card_image: {
+          flex:4,
+      },
+      title: {
+          fontSize: 20, 
+          fontWeight:'bold',
+      },
+      country: {
+          fontSize:12,
+          color: colors.violet
+      },
+      card_inner_view: {
+          borderRadius:15
+      },
 });
 
   
