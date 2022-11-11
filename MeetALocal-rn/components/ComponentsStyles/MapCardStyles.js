9760e62e0e939d725a1c77504488a8ae89dec283
noormesslmani <noor.messlmani@gmail.com>
2022-11-11 22:27:48 +0200

    Frontend: created a reusable user card to be displayed on map

---
+++ MeetALocal-rn/components/ComponentsStyles/MapCardStyles.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { widths } from "../../constants/dimensions";
+import { colors } from "../../constants/colors";
+import { color } from "react-native-reanimated";
+const MapCardStyles = StyleSheet.create({
+    card_scroll_view: {
+        position:'absolute',
+        bottom:10,
+        left:10,
+        right:0,
+        paddingVertical:8
+        },
+        card_view: {
+        elevation:90,
+        backgroundColor:'#fff',
+        borderRadius:10,
+        marginHorizontal: 10,
+        height: 200,
+        width: 250,
+        overflow: 'hidden'
+        },
+        card_image: {
+            flex:4,
+        },
+        title: {
+            fontSize: 20, 
+            fontWeight:'bold',
+        },
+        country: {
+            fontSize:12,
+            color: colors.violet
+        },
+        card_inner_view: {
+            borderRadius:15
+        },
+   
+});
+export default MapCardStyles;
