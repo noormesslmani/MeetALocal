bf8eb700c4d4026efa387fe4e27cb73501c154a7
noormesslmani <noor.messlmani@gmail.com>
2022-11-12 03:24:41 +0200

    Frontend: created a custom hook

---
+++ MeetALocal-rn/hooks/Hooks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {useEffect, useRef } from "react";
+export const useDidMountEffect = (func, deps) => {
+    const didMount = useRef(false);
+    useEffect(() => {
+        if (didMount.current) func();
+        else didMount.current = true;
+    }, deps);
+}
