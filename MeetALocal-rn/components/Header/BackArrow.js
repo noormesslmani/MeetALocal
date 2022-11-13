913e7987a056abdc0d8ee8c30ee0512cb1ec77c2
noormesslmani <noor.messlmani@gmail.com>
2022-11-14 00:24:32 +0200

    Frontend: replaced back arrow in navigation page with reusable component

--- MeetALocal-rn/components/Header/BackArrow.js
+++ MeetALocal-rn/components/Header/BackArrow.js
@@ -2,7 +2,7 @@ import { View, Text, TouchableOpacity, Image, SafeAreaView, FlatList, Pressable 
 import React from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { colors } from '../../constants/colors'
-const BackArrow=({navigation})=>{
-    return (<Pressable onPress={() => navigation.goBack()}><Ionicons name="chevron-back" size={30} color={colors.violet}/></Pressable>)
+const BackArrow=({navigation, type})=>{
+    return (<Pressable onPress={() => navigation.goBack()}><Ionicons name="chevron-back" size={30} color={type==1?(colors.violet):"white"}/></Pressable>)
 }
 export default BackArrow
