b440986b9a10047c40d5f6808e18fb794a030ae4
noormesslmani <noor.messlmani@gmail.com>
2022-11-13 05:34:31 +0200

    Frontend: created foreigners statistics page

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home';
 import Locals from './Pages/Users/Locals';
 import Foreigners from './Pages/Users/Foreigners';
 import LocalStatistics from './Pages/Statistics/LocalStatistics';
+import ForeignerStatistics from './Pages/Statistics/ForeignerStatistics';
 function App() {
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
           <Route path='/locals' exact element={<Locals/>} />
           <Route path='/foreigners' exact element={<Foreigners/>} />
           <Route path='/locals-statistics' exact element={<LocalStatistics/>} />
+          <Route path='/foreigners-statistics' exact element={<ForeignerStatistics/>} />
         </Routes>
       </Router>
     </>
