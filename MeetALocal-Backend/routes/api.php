c9c7358737a83611785186ed14589ec28feb2a39
noormesslmani <noor.messlmani@gmail.com>
2022-11-14 18:24:06 +0200

    Backend: created search locals api

--- MeetALocal-Backend/routes/api.php
+++ MeetALocal-Backend/routes/api.php
@@ -32,7 +32,7 @@ Route::group(['prefix' => 'v1.0.0'], function () {
         });
         Route::group(['prefix' => 'admins'], function () {
             Route::post('/toggle-ban', [AdminController::class, 'toggleBan']);
-            Route::get('/get-bans', [AdminController::class, 'getBans']);
+            Route::get('/bans', [AdminController::class, 'getBans']);
             Route::get('/app-stat', [AdminController::class, 'getAppStat']);
             Route::get('/locals-stat', [AdminController::class, 'getLocalsStat']);
             Route::get('/foreigners-stat', [AdminController::class, 'getForeignersStat']);
@@ -42,12 +42,13 @@ Route::group(['prefix' => 'v1.0.0'], function () {
             Route::get('/favorites', [ForeignerController::class, 'getFavorites']);
             Route::get('/is-favorite/{id}', [ForeignerController::class, 'isFavorite']);
             Route::post('/toggle-favorite', [ForeignerController::class, 'toggleFavorite']);
-            Route::post('/event/toggle-save', [UserController::class, 'toggleSavedEvents']);
-            Route::get('/event/is-saved/{id}', [UserController::class, 'isSaved']);
-            Route::get('/events/saved', [UserController::class, 'getSavedEvents']);
+            Route::post('/event/toggle-save', [ForeignerController::class, 'toggleSavedEvents']);
+            Route::get('/event/is-saved/{id}', [ForeignerController::class, 'isSaved']);
+            Route::get('/events/saved', [ForeignerController::class, 'getSavedEvents']);
             Route::post('/review', [ForeignerController::class, 'addReview']);
             Route::delete('/review', [ForeignerController::class, 'deleteReview']);
             Route::get('/is-reviewed', [ForeignerController::class, 'isReviewed']);
+            Route::get('/search', [ForeignerController::class, 'getSearch']);
         });
         Route::group(['prefix' => 'auth'], function ($router) { 
             Route::post('/logout', [AuthController::class, 'logout']);
