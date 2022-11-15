00128c532909c2738af357e612b13c0dd45f82bf
noormesslmani <noor.messlmani@gmail.com>
2022-11-15 21:36:17 +0200

    Frontend: implemented toggle booked event functionality

--- MeetALocal-rn/network/App.js
+++ MeetALocal-rn/network/App.js
@@ -390,3 +390,20 @@ export async function getLocals(country, category, offset){
       return {'success': false, error}
     }
   }
+  export async function toggleBookedEvent(data){
+    const token = await AsyncStorage.getItem('@token')
+    const config = {
+      method: "post",
+      data,
+      headers: { Authorization: `Bearer ${token}`},
+      url:`${baseURL}foreigners/toggle-event-booking`,
+    }
+    try{
+      const res = await axios(config)
+      return {success:true, data: res.data}
+    }
+    catch (error) {
+      console.warn(error)
+      return {'success': false, error}
+    }
+  }
