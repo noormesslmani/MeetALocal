3e3cdfde44446b0ca0a5e7046d79061a81fd0221
noormesslmani <noor.messlmani@gmail.com>
2022-11-15 14:10:57 +0200

    Frontend: implemeted is booked api and added booking button

--- MeetALocal-rn/components/Modals/EventModal.js
+++ MeetALocal-rn/components/Modals/EventModal.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image, Modal, Pressable } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Modal, Pressable, ScrollView } from 'react-native'
 import React from 'react'
 import { useState, useEffect, useContext } from "react";
 import EventModalStyles from '../ComponentsStyles/EventModalStyles';
@@ -9,15 +9,22 @@ import { toggleSaveEvent, isEventSaved } from '../../network/App';
 import { UserContext } from '../../App'
 import { deleteEvents } from '../../network/App';
 import { colors } from '../../constants/colors';
+import { Button} from 'react-native-paper';
+import EventsModalStyles from '../ComponentsStyles/EventsModalStyles';
+import { isEventBooked } from '../../network/App';
 const EventModal=({navigation, modalVisible, setModalVisible, item, choice, setDeleted})=> {
     const { user, setUser} = useContext(UserContext);
     const [categories, setCategories]=useState([])
     const [isSaved, setIsSaved]=useState(false)
+    const [isBooked, setIsBooked]=useState(false)
     useEffect(()=>{
         if(modalVisible)
         {
           setCategories(item.categories)
-          if(user.type_id==2){isSavedEvent()}
+          if(user.type_id==2){
+            isSavedEvent()
+            isBookedEvent()
+          }
         }
     },[modalVisible])
    
@@ -39,6 +46,12 @@ const EventModal=({navigation, modalVisible, setModalVisible, item, choice, setD
       const result = await isEventSaved(item.id)
       if (result.success){
         result.data.data? setIsSaved(true): setIsSaved(false)
+      }
+    }
+    const isBookedEvent= async()=>{
+      const result = await isEventBooked(item.id)
+      if (result.success){
+        result.data.data? setIsBooked(true): setIsBooked(false)
       }
     }
 
@@ -63,6 +76,7 @@ const EventModal=({navigation, modalVisible, setModalVisible, item, choice, setD
         <View style={EventModalStyles.centeredView}>
         <View style={EventModalStyles.modalView}>
             <Image source={item.photo?{ uri:`${address}/${item.photo}`}: require('../../assets/blank-profile.webp')} style={EventModalStyles.image}/>
+            <ScrollView showsVerticalScrollIndicator={false}>
             <View style={EventModalStyles.titleContainer}>
               <Text style={EventModalStyles.title}>{item.title}</Text>
               {user.type_id==2 && <Pressable onPress={handleSave}>
@@ -80,6 +94,16 @@ const EventModal=({navigation, modalVisible, setModalVisible, item, choice, setD
                 <Text style={EventModalStyles.fees}>Fees: {item.fees}$</Text>
               </View>
               <Text>Seats: {item.seats - item.bookings}</Text>
+              {user.type_id==2 && ((item.seats - item.bookings) >0) && !isBooked &&
+                <Button compact uppercase={false} labelStyle={{ color: colors.violet, fontSize: 16 }} style={EventModalStyles.bookBtn} icon={()=><Icon name='calendar' color={colors.violet} size={18} />}  mode="outlined" > 
+                Book
+               </Button>
+              }
+              {user.type_id==2 && isBooked &&
+                <Button compact uppercase={false} labelStyle={{ color: colors.violet, fontSize: 16 }} style={EventModalStyles.bookBtn} icon={()=><Icon name='calendar' color={colors.violet} size={18} />}  mode="outlined" > 
+                Cancel Booking
+               </Button>
+              }
               {choice==3 && <View style={{flexDirection:"row", alignItems:"center"}}><Icon style={{margin:2}} name="star" color={colors.gold} size={20} /><Text>: {item.saves}</Text></View>}
             </View>
             <View style={EventModalStyles.detailsContianer}>
@@ -92,7 +116,7 @@ const EventModal=({navigation, modalVisible, setModalVisible, item, choice, setD
               {categories.map((category)=><Image source={categoryIcons[category]} style={EventModalStyles.icons} />)}
               </View>
             </View>
-            
+          </ScrollView> 
         </View>
       </View>
     </Modal>
