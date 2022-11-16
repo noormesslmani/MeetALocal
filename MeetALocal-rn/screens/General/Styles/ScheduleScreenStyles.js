67f072ea37b42be735b5969ad7c62a20b47f46dd
noormesslmani <noor.messlmani@gmail.com>
2022-11-16 03:17:18 +0200

    Frontend: added some styling to the schedules screen

--- MeetALocal-rn/screens/General/Styles/ScheduleScreenStyles.js
+++ MeetALocal-rn/screens/General/Styles/ScheduleScreenStyles.js
@@ -5,11 +5,36 @@ import { widths } from "../../../constants/dimensions";
 const ScheduleStyles = StyleSheet.create({
     container:{
         alignItems:"center",
-        paddingVertical:40,
+        paddingTop:80,
         width:widths.width,
     },
     list:{
         width:widths.width9,
     },
+    labelsContainer:{
+        flexDirection:"row",
+        width:widths.width6,
+        justifyContent:"space-between",
+        alignItems:"center"
+    },
+    label:{
+        flexDirection:"row",
+        alignItems:"center"
+    },
+    circle:{
+        height:16,
+        width:16,
+        borderRadius:8,
+        shadowColor: "#000",
+        backgroundColor:"white",
+        shadowOffset: {
+            width: 2,
+            height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 1,
+        elevation: 3,
+        margin:5
+    }
 });
 export default ScheduleStyles;
