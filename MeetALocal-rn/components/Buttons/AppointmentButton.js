e0798faabe9e5c0cd8ff18e081f895aaaa078e5a
noormesslmani <noor.messlmani@gmail.com>
2022-11-16 04:42:37 +0200

    Frontend: restyled appointment button

--- MeetALocal-rn/components/Buttons/AppointmentButton.js
+++ MeetALocal-rn/components/Buttons/AppointmentButton.js
@@ -3,27 +3,41 @@ import React, { useState } from 'react'
 import { widths } from '../../constants/dimensions'
 import { colors } from '../../constants/colors'
 import { Button} from 'react-native-paper';
-const AppointmentButton=({appointment})=>{
-    const [pressed, setPressed]=useState(false)
+const AppointmentButton=({appointment, setSelected, selected})=>{
+    
     return (
-        <Button uppercase={false} onPress={()=>setPressed(!pressed)} labelStyle={{ color: colors.violet, fontSize: 16 }} style={[styles.bookBtn,pressed? styles.pressedBtn:null]}   mode="outlined" > 
-                {appointment.date}   {appointment.start_time.substring(0, 5)}: {appointment.end_time.substring(0, 5)}
-        </Button>
+        <TouchableOpacity  onPress={()=>setSelected(appointment)} style={[styles.bookBtn, selected==appointment? styles.pressedBtn:null]} > 
+                <Text style={styles.date}>{appointment.date} </Text>  
+                <Text>{appointment.start_time.substring(0, 5)}:{appointment.end_time.substring(0, 5)}</Text>
+        </TouchableOpacity>
     )
 }
 export default AppointmentButton
 const styles=StyleSheet.create({
     bookBtn:{
         marginVertical:10,
+        flexDirection:"row",
         width:widths.width7,
         height:55,
+        paddingHorizontal:0.1*widths.width,
+        justifyContent:"space-between",
         backgroundColor:'white',
         borderColor:colors.violet,
         borderRadius:10,
         alignItems:"center",
-        justifyContent:"center"
+        shadowOffset: {
+            width: 0,
+            height: 2
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 4,
+          elevation: 3
     },
     pressedBtn:{
-        backgroundColor:colors.lighterViolet, 
+        backgroundColor:colors.lightViolet, 
+    },
+    date:{
+        fontSize:16,
+        fontWeight:"600"
     }
 })
