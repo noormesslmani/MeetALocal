f379274436fbc5cb5fc17347712152f84bed2154
noormesslmani <noor.messlmani@gmail.com>
2022-11-17 20:59:13 +0200

    Backend: used query parameters in some apis

--- MeetALocal-Backend/app/Http/Controllers/UserController.php
+++ MeetALocal-Backend/app/Http/Controllers/UserController.php
@@ -25,10 +25,13 @@ use Illuminate\Support\Facades\Auth;
 use Validator;
 class UserController extends Controller
 {
-    public function getLocals($country, $category, $offset){
+    public function getLocals(Request $request){
+        $country=$request->query('country');
+        $category=$request->query('category');
+        $offset=$request->query('offset');
         $country!='all'? $country_id= Country::where('country',$country)->pluck('id'):$country_id=Country::pluck('id');
         $category!='all'? $category_id=Category::where('category',$category)->pluck('id'):$category_id=Category::pluck('id');
-        $locals= User::join('local_categories','users.id','=','local_id')->join('categories','local_categories.category_id','=','categories.id')->join('countries','users.residence_id','=','countries.id')->where('type_id',1)->whereIn('users.residence_id',$country_id)->whereIn('local_categories.category_id',$category_id)->select('users.*','countries.country')->distinct()->orderBy('created_at', 'desc')->offset($offset)->limit(15)->get();
+        $locals= User::join('local_categories','users.id','=','local_id')->join('categories','local_categories.category_id','=','categories.id')->join('countries','users.residence_id','=','countries.id')->where('type_id',1)->whereIn('users.residence_id',$country_id)->whereIn('local_categories.category_id',$category_id)->distinct()->orderBy('created_at', 'desc')->offset($offset)->limit(15)->get(['users.*','countries.country']);
         foreach($locals as $local){
             $local['likes']=FavoriteLocal::where('local_id',$local->id)->count();
             $local['categories']=$local->categories()->pluck('category');
@@ -47,7 +50,9 @@ class UserController extends Controller
             'data' => $user
         ], 201);
     }
-    public function getEvents($country, $category){
+    public function getEvents(Request $request){
+        $country=$request->query('country');
+        $category=$request->query('category');
         $date = today()->format('Y-m-d');
         $country!='all'? $country_id= Country::where('country',$country)->pluck('id'):$country_id=Country::pluck('id');
         $category!='all'? $category_id=Category::where('category',$category)->pluck('id'):$category_id=Category::pluck('id');
@@ -74,7 +79,10 @@ class UserController extends Controller
         ], 201);
     }
     
-    public function getPosts($country, $category, $offset){
+    public function getPosts(Request $request){
+        $country=$request->query('country');
+        $category=$request->query('category');
+        $offset=$request->query('offset');
         $country!='all'? $country_id= Country::where('country',$country)->pluck('id'):$country_id=Country::pluck('id');
         $category!='all'? $category_id=Category::where('category',$category)->pluck('id'):$category_id=Category::pluck('id');
         $posts= Post::join('post_categories','posts.id','=','post_id')->join('categories','post_categories.category_id','=','categories.id')->join('countries','posts.country_id','=','countries.id')->join('users','posts.user_id','=','users.id')->whereIn('posts.country_id',$country_id)->whereIn('post_categories.category_id',$category_id)->orderBy('posts.id', 'desc')->select('posts.*','countries.country','users.name')->distinct()->orderBy('created_at', 'desc')->offset($offset)->limit(20)->get();
