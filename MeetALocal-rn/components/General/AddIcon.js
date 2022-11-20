44f77d488122fc3aa65c07121ca0e4a446a490ee
noormesslmani <noor.messlmani@gmail.com>
2022-11-20 19:37:16 +0200

    Frontend: created add icon component

---
+++ MeetALocal-rn/components/General/AddIcon.js
@@ -0,0 +1,13 @@
+import {TouchableOpacity } from 'react-native'
+import React from 'react'
+import { colors } from '../../constants/colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AddIconStyle from './Styles/AddIconStyle';
+const AddIcon=({handlePress})=>{
+    return(
+        <TouchableOpacity onPress={handlePress} style={AddIconStyle.addIcon} >
+            <Icon name= 'plus' size={50} color={colors.lightViolet} />
+        </TouchableOpacity>
+    )
+}
+export default AddIcon
