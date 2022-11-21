05095fdef28db3f37c33c63ef5d44706f15b2300
noormesslmani <noor.messlmani@gmail.com>
2022-11-21 02:29:51 +0200

    Frontend: created schedule modal and renamed files

---
+++ MeetALocal-rn/components/Modals/NewScheduleModal.js
@@ -0,0 +1,112 @@
+import { View, Text, TouchableOpacity, Modal, ActivityIndicator } from 'react-native'
+import React from 'react'
+import { useState, useEffect, useContext } from "react";
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { colors } from '../../constants/colors';
+import DatePicker from '../General/datePicker';
+import TimePicker from '../General/TimePicker';
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import AppButton from '../Buttons/AppButtons';
+import { addSchedule } from '../../network/App';
+import ScheduleModalStyle from './Styles/NewScheduleModalStyle';
+import { UserContext } from '../../App'
+const ScheduleModal=({setModalVisible, modalVisible, setScheduleAdded, navigation })=> {
+    const { user, setUser} = useContext(UserContext);
+    const [date, setDate]=useState(new Date())
+    const [startTime, setStartTime]= useState(new Date() )
+    const [endTime, setEndTime]= useState(new Date() )
+    const [startTimePicker, setStartTimePicker]= useState(false)
+    const [endTimePicker, setEndTimePicker]= useState(false)
+    const [datePicker, setDatePicker]= useState(false)
+    const [isLoading, setIsloading]=useState(false)
+    const[latitude, setLatitude]=useState(user.latitude)
+    const[longitude, setLongitude]=useState(user.longitude)
+    const handleDate=(event, value)=>{
+        setDatePicker(false)
+        if(event.type == "set") {
+            setDate(value)
+        }   
+    }
+    const handleStartTime=(event, value)=>{
+        setStartTimePicker(false)
+        if(event.type == "set") {
+            setStartTime(value)
+        }   
+    }
+    const handleEndTime=(event, value)=>{
+        setEndTimePicker(false)
+        if(event.type == "set") {
+            console.log(value)
+            setEndTime(value)
+        }   
+    }
+    const handleSubmit=async ()=>{
+        if(endTime>startTime){
+        setIsloading(true)
+        const data={
+            date:`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`,
+            start_time:`${startTime.getHours()}:${startTime.getMinutes()}`,
+            end_time:`${endTime.getHours()}:${endTime.getMinutes()}`,
+            latitude,
+            longitude
+        }
+        console.log(data)
+        const result = await addSchedule(data)
+        if (result.success){
+            console.log(result.data.data)
+            setModalVisible(false)
+            setScheduleAdded(true)
+        }
+        }
+        setIsloading(false)
+    }
+
+  return (
+    <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+        setModalVisible(!modalVisible);
+        }}>
+        <View style={ScheduleModalStyle.centeredView}>
+        <View style={ScheduleModalStyle.modalView}>
+        
+            <Text style={ScheduleModalStyle.modalTitle}>Add an appointment</Text> 
+            <View style={{flexDirection:"row", alignItems:"flex-start"}}>
+
+            <View style={ScheduleModalStyle.dateContainer}>
+            <Text style={ScheduleModalStyle.title}>Date</Text>
+            <TouchableOpacity onPress={()=>setDatePicker(true)}>
+                <Icon name="calendar" size={30} color={colors.mediumViolet}/></TouchableOpacity>
+                <Text style={ScheduleModalStyle.text}>{date.getFullYear()}-{date.getMonth()+1}-{date.getDate()}</Text>
+            </View>
+            
+            <View style={ScheduleModalStyle.dateContainer}>
+            <Text style={ScheduleModalStyle.title}>Start</Text>
+            <TouchableOpacity onPress={()=>setStartTimePicker(true)}>
+                <AntDesign name="clockcircleo" size={30} color={colors.mediumViolet}/></TouchableOpacity>
+                <Text style={ScheduleModalStyle.text}>{startTime.getHours()}:{startTime.getMinutes()}</Text>
+            </View>
+            
+            <View style={ScheduleModalStyle.dateContainer}>
+            <Text style={ScheduleModalStyle.title}>End</Text>
+            <TouchableOpacity onPress={()=>setEndTimePicker(true)}><AntDesign name="clockcircleo" size={30} color={colors.mediumViolet}/></TouchableOpacity>
+            <Text style={ScheduleModalStyle.text}>{endTime.getHours()}:{endTime.getMinutes()}</Text>
+            </View>
+            
+            </View>
+            {datePicker && <DatePicker date={date} handleDate={handleDate} type={2} />}
+            {startTimePicker && <TimePicker time={startTime} handleTime={handleStartTime}/> }
+            {endTimePicker && <TimePicker time={endTime} handleTime={handleEndTime}/> }
+            {isLoading && <ActivityIndicator color={colors.violet} /> }
+            <View style={ScheduleModalStyle.ButtonContianer}>
+                <AppButton text={'Submit'} handlePress={handleSubmit} />
+                <AppButton text={'Cancel'} handlePress={()=>setModalVisible(false)} />
+            </View>
+        </View>
+      </View>
+    </Modal>
+  )
+}
+export default ScheduleModal
--- MeetALocal-rn/components/Modals/ScheduleModal.js
+++ MeetALocal-rn/components/Modals/ScheduleModal.js
@@ -1,61 +1,17 @@
-import { View, Text, TouchableOpacity, Modal, ActivityIndicator } from 'react-native'
 import React from 'react'
+import { View, Text, TouchableOpacity, Modal, Image, Pressable } from 'react-native'
 import { useState, useEffect, useContext } from "react";
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { colors } from '../../constants/colors';
-import DatePicker from '../General/datePicker';
-import TimePicker from '../General/TimePicker';
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import AppButton from '../Buttons/AppButtons';
-import { addSchedule } from '../../network/App';
+import { address } from '../../constants/address';
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import ScheduleModalStyle from './Styles/ScheduleModalStyle';
+import { UserContext } from '../../App'
+const ScheduleModal=({setModalVisible, modalVisible, item})=> {
+    const { user, setUser} = useContext(UserContext);
+    console.log(item.booker)
 
-const ScheduleModal=({setModalVisible, modalVisible, setScheduleAdded })=> {
-    const [date, setDate]=useState(new Date())
-    const [startTime, setStartTime]= useState(new Date() )
-    const [endTime, setEndTime]= useState(new Date() )
-    const [startTimePicker, setStartTimePicker]= useState(false)
-    const [endTimePicker, setEndTimePicker]= useState(false)
-    const [datePicker, setDatePicker]= useState(false)
-    const [isLoading, setIsloading]=useState(false)
-    const handleDate=(event, value)=>{
-        setDatePicker(false)
-        if(event.type == "set") {
-            setDate(value)
-        }   
-    }
-    const handleStartTime=(event, value)=>{
-        setStartTimePicker(false)
-        if(event.type == "set") {
-            setStartTime(value)
-        }   
-    }
-    const handleEndTime=(event, value)=>{
-        setEndTimePicker(false)
-        if(event.type == "set") {
-            console.log(value)
-            setEndTime(value)
-        }   
-    }
-    const handleSubmit=async ()=>{
-        if(endTime>startTime){
-        setIsloading(true)
-        const data={
-            date:`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`,
-            start_time:`${startTime.getHours()}:${startTime.getMinutes()}`,
-            end_time:`${endTime.getHours()}:${endTime.getMinutes()}`,
-        }
-        console.log(data)
-        const result = await addSchedule(data)
-        if (result.success){
-            console.log(result.data.data)
-            setModalVisible(false)
-            setScheduleAdded(true)
-        }
-        }
-        setIsloading(false)
-    }
-    
   return (
     <Modal
         animationType="fade"
@@ -66,40 +22,25 @@ const ScheduleModal=({setModalVisible, modalVisible, setScheduleAdded })=> {
         }}>
         <View style={ScheduleModalStyle.centeredView}>
         <View style={ScheduleModalStyle.modalView}>
+        <Pressable style={{position:"absolute", right:10, top:10}} onPress={()=>setModalVisible(false)} ><Ionicons name='close' size={20} /></Pressable>
+        <Text style={ScheduleModalStyle.modalTitle} >Appointment Details</Text>
+        <View style={ScheduleModalStyle.detailsContainer}>
+            <Icon name='calendar' size={30} color={colors.lightViolet} style={ScheduleModalStyle.icon} />
+            <Text style={ScheduleModalStyle.text}>{item.date}</Text>
+        </View>
+        <View style={ScheduleModalStyle.detailsContainer}>
+            <AntDesign name='clockcircleo' size={30} color={colors.lightViolet} style={ScheduleModalStyle.icon}/>
+            <Text style={ScheduleModalStyle.text}>{item.start_time} - {item.end_time}</Text>
+        </View>
+        <View style={ScheduleModalStyle.detailsContainer}>
+            <Icon name='user' size={30} color={colors.lightViolet} style={ScheduleModalStyle.icon}/>
+            {item.booker.length==0 && <Text style={ScheduleModalStyle.text}>Not Booked Yet</Text>}
+            {item.booker.length>0 &&<View style={ScheduleModalStyle.bookerContainer} >
+                <Image source={item.booker[0].profile_picture?{ uri:`${address}/${item.booker[0].profile_picture}`}: require('../../assets/blank-profile.webp')} style={ScheduleModalStyle.profileImage} />
+                <Text style={ScheduleModalStyle.text}>{item.booker[0].name}</Text>
+            </View> }
+        </View>
         
-            <Text style={ScheduleModalStyle.modalTitle}>Add an appointment</Text> 
-            <View style={{flexDirection:"row", alignItems:"flex-start"}}>
-
-            <View style={ScheduleModalStyle.dateContainer}>
-            <Text style={ScheduleModalStyle.title}>Date</Text>
-            <TouchableOpacity onPress={()=>setDatePicker(true)}>
-                <Icon name="calendar" size={30} color={colors.mediumViolet}/></TouchableOpacity>
-                <Text style={ScheduleModalStyle.text}>{date.getFullYear()}-{date.getMonth()+1}-{date.getDate()}</Text>
-            </View>
-            
-            <View style={ScheduleModalStyle.dateContainer}>
-            <Text style={ScheduleModalStyle.title}>Start</Text>
-            <TouchableOpacity onPress={()=>setStartTimePicker(true)}>
-                <AntDesign name="clockcircleo" size={30} color={colors.mediumViolet}/></TouchableOpacity>
-                <Text style={ScheduleModalStyle.text}>{startTime.getHours()}:{startTime.getMinutes()}</Text>
-            </View>
-            
-            <View style={ScheduleModalStyle.dateContainer}>
-            <Text style={ScheduleModalStyle.title}>End</Text>
-            <TouchableOpacity onPress={()=>setEndTimePicker(true)}><AntDesign name="clockcircleo" size={30} color={colors.mediumViolet}/></TouchableOpacity>
-            <Text style={ScheduleModalStyle.text}>{endTime.getHours()}:{endTime.getMinutes()}</Text>
-            </View>
-            
-            </View>
-            
-            {datePicker && <DatePicker date={date} handleDate={handleDate} type={2} />}
-            {startTimePicker && <TimePicker time={startTime} handleTime={handleStartTime}/> }
-            {endTimePicker && <TimePicker time={endTime} handleTime={handleEndTime}/> }
-            {isLoading && <ActivityIndicator color={colors.violet} /> }
-            <View style={ScheduleModalStyle.ButtonContianer}>
-                <AppButton text={'Submit'} handlePress={handleSubmit} />
-                <AppButton text={'Cancel'} handlePress={()=>setModalVisible(false)} />
-            </View>
         </View>
       </View>
     </Modal>
