b0a71ea3198e3311930d182a61b8eadf9c5e038f
noormesslmani <noor.messlmani@gmail.com>
2022-11-21 14:50:05 +0200

    Frontend: moved Logo component to header folder

--- MeetALocal-rn/components/Header/Logo.js
+++ MeetALocal-rn/components/Header/Logo.js
@@ -1,11 +1,11 @@
 import { Image } from 'react-native'
 import React from 'react'
-import styles from './styles'
+import LogoStyle from './Styles.js/LogoStyle'
 
 const Logo= ()=> {
     return (
         <Image
-        style={styles.logo}
+        style={LogoStyle.logo}
         source={require('../../assets/logo.png')}/>
     )
   }
