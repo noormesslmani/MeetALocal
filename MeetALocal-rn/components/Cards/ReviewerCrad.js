24d7f2a1be1caf7c7cb00c34e2b4d0495c04fa36
noormesslmani <noor.messlmani@gmail.com>
2022-11-22 22:08:47 +0200

    Frontend: implemented delete review functionality

--- MeetALocal-rn/components/Cards/ReviewerCrad.js
+++ MeetALocal-rn/components/Cards/ReviewerCrad.js
@@ -1,14 +1,19 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Pressable } from 'react-native'
 import React from 'react'
 import { address } from '../../constants/address';
+import { useContext } from "react";
 import { Rating} from 'react-native-ratings';
 import ReviewerCardStyle from './Styles/ReviewerCardStyle';
-const ReviewCard=({review})=> {
-
+import { UserContext } from '../../App';
+import Icon from 'react-native-vector-icons/FontAwesome'
+const ReviewCard=({review, hanldeDelete})=> {
+  const { user, setUser} = useContext(UserContext);
+  console.log(review)
   return (<View style={ReviewerCardStyle.cotainer}>
+            {user.id==review.reviewer_id && <Pressable onPress={hanldeDelete} style={ReviewerCardStyle.trash} ><Icon name='trash' size={20} color='grey' /></Pressable>}
             <View style={ReviewerCardStyle.imageContianer}>
                 <Image style={ReviewerCardStyle.image} source={review.profile_picture?{ uri:`${address}/${review.profile_picture}`}: require('../../assets/blank-profile.webp')}/>
-                <Text style={ReviewerCardStyle.name}>{review.name}</Text>
+                <Text style={ReviewerCardStyle.name}>{user.id==review.reviewer_id? 'You': review.name}</Text>
                 <Rating size={10} startingValue={review.stars} readonly= {true}  imageSize={10}/>
             </View>
             <Text style={ReviewerCardStyle.review}>{review.review}</Text>
