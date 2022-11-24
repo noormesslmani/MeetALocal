7b5d32dbae0754ec74efdc6796568d5a4bf7b1a5
noormesslmani <noor.messlmani@gmail.com>
2022-11-24 23:48:06 +0200

    Frontend: used helper text for validation

--- MeetALocal-rn/screens/Auth/SignupScreen3.js
+++ MeetALocal-rn/screens/Auth/SignupScreen3.js
@@ -2,13 +2,14 @@ import { View, Text, ActivityIndicator } from 'react-native'
 import React from 'react'
 import styles from './Styles/AuthScreensStyle';
 import { useState, useEffect } from "react";
-import { TextInput } from 'react-native-paper';
+import { TextInput, HelperText } from 'react-native-paper';
 import AuthButton from '../../components/Buttons/AuthButton';
 import BackArrow from '../../components/Header/BackArrow';
 import Logo from '../../components/Header/Logo'
 import { useRoute } from '@react-navigation/native';
 import { colors } from '../../constants/colors';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
 const SignupScreen3 = ({navigation}) => {
   const route = useRoute();
   const fullName= route.params.fullName
@@ -19,11 +20,7 @@ const SignupScreen3 = ({navigation}) => {
   const language = route.params.language
   const [email, setEmail]=useState('');
   const [password, setPassword]=useState('');
-  const [confirmPassword, setConfirmPassword]=useState([]);
-  const [invalidEmail, setInvalidEmail]= useState(false)
-  const [invalidPassword, setInvalidPassword]= useState(false)
-  const [unmatchedPassword, setUnmatchedPassword]= useState(false)
-  const [isLoading, setIsLoading]= useState(false)
+  const [confirmPassword, setConfirmPassword]=useState('');
   const emailFormat=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   const passFormat= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
   useEffect(() => {
@@ -33,65 +30,61 @@ const SignupScreen3 = ({navigation}) => {
       });
   }, [navigation]);
   const handleNext=()=>{
-    setInvalidEmail(false)
-    setInvalidPassword(false)
-    setUnmatchedPassword(false)
-    if(! email.match(emailFormat))
-    {
-      setInvalidEmail(true)
-      setTimeout(() => {
-        setInvalidEmail(false);
-      }, 1500);
-    }
-    else if(! password.match(passFormat)){
-      setInvalidPassword(true)
-      setTimeout(() => {
-        setInvalidPassword(false);
-      }, 3000);
-    }
-    else if(password!=confirmPassword){
-      setUnmatchedPassword(true)
-      setTimeout(() => {
-        setUnmatchedPassword(false);
-      }, 1500);
-    }
-    else{
+    if(email.match(emailFormat) && password.match(passFormat) && confirmPassword==password ){
       navigation.navigate('user-type',{fullName,email,password,nationality,country,phone,dob,language})
     }
   }
  
+  const emailHasErrors = () => {
+    return !email.match(emailFormat) && !email==''
+  };
+
+  const passwordHasErrors = () => {
+    return !password.match(passFormat) && !password==''
+  };
+
+  const confirmationHasErrors=()=>{
+    return confirmPassword!=password && !confirmPassword=='' 
+  }
+
   return (
     <View style={styles.background}>
       <KeyboardAwareScrollView style={styles.scrollView} scrollEnabled={false}  showsVerticalScrollIndicator={false}>
         <View style={styles.mainContainer}>
-        <View style={[styles.formContainer, styles.shadowProp, styles.signUp]}>
-          <Text style={styles.signIn}>Register</Text>
-          <View style={styles.inputContainer}>
-            <TextInput label="Email" style={styles.input} onChangeText={setEmail} value={email}
-            left={<TextInput.Icon icon="email" />} underlineColor={colors.lightViolet} activeUnderlineColor={colors.mediumViolet}></TextInput>
-            {invalidEmail && <Text style={styles.error}>Please enter a valid email</Text>}
+          <View style={[styles.formContainer, styles.shadowProp, styles.signUp]}>
+            <Text style={styles.signIn}>Register</Text>
+            <View style={styles.inputContainer}>
+              <TextInput label="Email" style={styles.input} onChangeText={setEmail} value={email}
+              left={<TextInput.Icon icon="email" />} underlineColor={colors.lightViolet} activeUnderlineColor={colors.mediumViolet}/>
+                <HelperText type="error" visible={emailHasErrors()}>
+                  Email address is invalid!
+                </HelperText>
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput left={<TextInput.Icon icon="lock" />}
+              secureTextEntry={true} label="Password" style={styles.input} onChangeText={setPassword} value={password}
+              underlineColor={colors.lightViolet} activeUnderlineColor={colors.mediumViolet}/>
+              <HelperText type="error" visible={passwordHasErrors()}>
+                Invalid Password
+              </HelperText>
+    
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput left={<TextInput.Icon icon="lock" />}
+              secureTextEntry={true} label="Confirm password" style={styles.input} onChangeText={setConfirmPassword} value={confirmPassword}
+              underlineColor={colors.lightViolet} activeUnderlineColor={colors.mediumViolet}/>
+              <HelperText type="error" visible={confirmationHasErrors()}>
+                Passwords do not match!
+              </HelperText>
+            </View>
+            <AuthButton title={'Next'} handleSubmit={handleNext} ></AuthButton>
+            <Text style={styles.text}>Already have an account?
+              </Text>
+              <Text style={styles.link} onPress={() => navigation.navigate('signin')}>
+                    Login
+            </Text> 
+            </View>
           </View>
-          <View style={styles.inputContainer}>
-            <TextInput left={<TextInput.Icon icon="lock" />}
-            secureTextEntry={true} label="Password" style={styles.input} onChangeText={setPassword} value={password}
-            underlineColor={colors.lightViolet} activeUnderlineColor={colors.mediumViolet}></TextInput>
-            {invalidPassword && <Text style={styles.error}>Password must contain atleast 8 characters, 1 uppercase letter, 1 lowercase letter, and 1 number</Text>}
-          </View>
-          <View style={styles.inputContainer}>
-            <TextInput left={<TextInput.Icon icon="lock" />}
-            secureTextEntry={true} label="Confirm password" style={styles.input} onChangeText={setConfirmPassword} value={confirmPassword}
-            underlineColor={colors.lightViolet} activeUnderlineColor={colors.mediumViolet}></TextInput>
-            {unmatchedPassword && <Text style={styles.error}>Passwords do not match!</Text>}
-          </View>
-          {isLoading && <ActivityIndicator color={colors.violet}/>}
-          <AuthButton title={'Next'} handleSubmit={handleNext} ></AuthButton>
-          <Text style={styles.text}>Already have an account?
-          </Text>
-          <Text style={styles.link} onPress={() => navigation.navigate('signin')}>
-                Login
-          </Text> 
-        </View>
-        </View>
         </KeyboardAwareScrollView>
     </View>
   )
