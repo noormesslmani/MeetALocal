4743895f20fd19242c2c82b351f0abdb7f65060b
noormesslmani <noor.messlmani@gmail.com>
2022-11-24 17:08:11 +0200

    Frontend: refactored notifications apis

--- MeetALocal-rn/network/Notifications.js
+++ MeetALocal-rn/network/Notifications.js
@@ -1,39 +1,20 @@
-import axios from 'axios';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { address } from '../constants/address';
+import response from './Response';
 const baseURL= `${address}/api/v1.0.0/`
 
 export async function saveToken(data){
-    const token = await AsyncStorage.getItem('@token')
     const config = {
       method: "put",
       data,
-      headers: { Authorization: `Bearer ${token}`},
       url:`${baseURL}users/token`,
     }
-    try{
-      const res = await axios(config)
-      return {success:true, data: res.data}
-    }
-    catch (error) {
-      console.warn(error)
-      return {'success': false, error}
-    }
+    return response(config)
   }
   export async function getToken(id){
-    const token = await AsyncStorage.getItem('@token')
     const config = {
       method: "get",
       params:{id},
-      headers: { Authorization: `Bearer ${token}`},
       url:`${baseURL}users/token`,
     }
-    try{
-      const res = await axios(config)
-      return {success:true, data: res.data}
-    }
-    catch (error) {
-      console.warn(error)
-      return {'success': false, error}
-    }
+    return response(config)
   }
