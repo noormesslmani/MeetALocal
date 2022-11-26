5f9befcfdd36f44f6956caef97d137677a3311d7
noormesslmani <noor.messlmani@gmail.com>
2022-11-26 02:54:20 +0200

    Frontend: added backarrow and navigation props to some screens

--- MeetALocal-rn/navigation/HomeStack.js
+++ MeetALocal-rn/navigation/HomeStack.js
@@ -1,6 +1,7 @@
 import { View, Pressable } from 'react-native';
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import BackArrow from '../components/Header/BackArrow';
 import Posts from '../screens/General/PostsScreen';
@@ -18,7 +19,7 @@ import EditLocation from '../screens/Profile/EditLocation';
 import Reviews from "../screens/General/Local'sReviews";
 import ForeignerPage from "../screens/General/Foreigner'sPage";
 const HomeStack=()=>{
-  
+  const navigation= useNavigation()
   const Stack = createNativeStackNavigator();
   return (
       <Stack.Navigator>
@@ -27,63 +28,63 @@ const HomeStack=()=>{
         <Stack.Screen name="comments"  component={PostComments} />
         <Stack.Screen  name="posts" component={Posts} 
           options={{ 
-            headerLeft: () => <BackArrow type={1}  />,
+            headerLeft: () => <BackArrow type={1}  navigation={navigation}/>,
             headerRight:()=>(<Pressable><Ionicons name="filter" size={25} color="#8C57BA"/></Pressable>),
             headerBackVisible:false, headerTitleAlign: 'center'}}/>
         <Stack.Screen  name="events" component={Events} options={{ 
-            headerLeft: () => <BackArrow type={1} />,
+            headerLeft: () => <BackArrow type={1} navigation={navigation}/>,
             headerRight:()=>(<Pressable><Ionicons name="filter" size={25} color="#8C57BA"/></Pressable>),
             headerBackVisible:false, headerTitleAlign: 'center'}} />
         <Stack.Screen  name="locals-map" component={LocalsMap} 
             options={{ 
-                headerLeft: () => <BackArrow type={1} />,
+                headerLeft: () => <BackArrow type={1} navigation={navigation}/>,
                 headerBackVisible:false, headerTitleAlign: 'center', headerTitle:'Map'}}
         />
         <Stack.Screen  name="chat-screen" component={ChatScreen} 
         options={{
-            headerLeft: () => (<BackArrow type={1} />),
+            headerLeft: () => (<BackArrow type={1} navigation={navigation}/>),
               headerBackVisible:false, headerTitle:""
           }}
         />
         <Stack.Screen  name="edit-foreigner-profile" component={EditForeignerProfile} 
         options={{
-          headerLeft: () => (<BackArrow type={1}  />),
+          headerLeft: () => (<BackArrow type={1} navigation={navigation} />),
             headerBackVisible:false, headerTitle:"Edit Profile", headerTitleAlign:"center"
         }}/>
         <Stack.Screen  name="edit-local-profile" component={EditLocalProfile}
         options={{
-          headerLeft: () => (<BackArrow type={1} />),
+          headerLeft: () => (<BackArrow type={1} navigation={navigation}/>),
             headerBackVisible:false, headerTitle:"Edit Profile", headerTitleAlign:"center"
         }} />
         <Stack.Screen  name="edit-location" component={EditLocation} 
         options={{
-          headerLeft: () => (<BackArrow type={1} />),
+          headerLeft: () => (<BackArrow type={1} navigation={navigation} />),
             headerBackVisible:false, headerTitle:"Edit Location", headerTitleAlign:"center"
         }}
         />
         <Stack.Screen  name="local-page" component={LocalPage}
         options={{
-          headerLeft: () => (<BackArrow type={2} />),
+          headerLeft: () => (<BackArrow type={2} navigation={navigation} />),
             headerBackVisible:false, headerTitle:"",
             headerStyle:{backgroundColor: colors.lighterViolet}, headerShadowVisible:false,
         }}
         />
         <Stack.Screen  name="foreigner-page" component={ForeignerPage}
         options={{
-          headerLeft: () => (<BackArrow type={2} />),
+          headerLeft: () => (<BackArrow type={2} navigation={navigation}/>),
             headerBackVisible:false, headerTitle:"",
             headerStyle:{backgroundColor: colors.lighterViolet}, headerShadowVisible:false,
         }}
         />
         <Stack.Screen  name="reviews" component={Reviews}
         options={{
-          headerLeft: () => (<BackArrow type={2} />),
+          headerLeft: () => (<BackArrow type={2} navigation={navigation}/>),
             headerBackVisible:false, headerTitle:"Reviews", headerTitleAlign:"center",
             headerShadowVisible:false,
         }}
         />
         <Stack.Screen  name="locals" component={Locals}   options={{ 
-            headerLeft: () => <BackArrow type={1}/>,
+            headerLeft: () => <BackArrow type={1} navigation={navigation}/>,
             headerRight:()=>(
               <View style={{flexDirection:"row"}}>
               <Pressable style={{marginLeft:10}}><Ionicons name="location-sharp" size={25} color={colors.violet} /></Pressable>
