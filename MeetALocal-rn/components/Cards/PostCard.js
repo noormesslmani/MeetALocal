87d8202b7499d7c6bf7a11843259494a1a908d9e
noormesslmani <noor.messlmani@gmail.com>
2022-11-28 03:55:12 +0200

    Frontend: implemented delete post functionality

--- MeetALocal-rn/components/Cards/PostCard.js
+++ MeetALocal-rn/components/Cards/PostCard.js
@@ -1,16 +1,24 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Pressable } from 'react-native';
 import React from 'react';
-import { useState } from "react";
+import { useState, useContext } from "react";
 import PostCardStyle from './Styles/PostCardStyle';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { address } from '../../constants/address';
 import { colors } from '../../constants/colors';
-const PostCard=({navigation, item})=> {
+import { EventsContext } from '../../context/EventsContext';
+import { deletePost } from '../../network/App';
+const PostCard=({navigation, item, viewOwn})=> {
+    const {posts, setPosts}=useContext(EventsContext);
     const [totalComments, setTotalComments]=useState(item.comments);
     const handlePost=()=>{
      navigation.navigate('comments',{item});
-}
-
+    }
+    const handleTrash=async()=>{
+        const result=await deletePost({id:item.id});
+        if(result.success){
+            setPosts(posts.filter(post=>post!=item));
+        }
+    }
   return (<>
     <TouchableOpacity style={PostCardStyle.card} onPress={handlePost}>
         <View style={PostCardStyle.headerContainer}>
@@ -22,7 +30,10 @@ const PostCard=({navigation, item})=> {
                         <Text style={PostCardStyle.name}>{item.name}</Text>
                         <Text style={PostCardStyle.country}>{item.country}</Text>
                         </View>
+                        <View style={{flexDirection:"row", alignItems:"center"}}>
                         <Text style={PostCardStyle.date}>{item.created_at.substring(0,10)}</Text>
+                        {viewOwn && <Pressable onPress={handleTrash}><Icon name='trash' size={20} color='grey' /></Pressable>}
+                        </View>
                     </View>
                     <Text style={PostCardStyle.postDetails}>{item.details}</Text>
                 </View>
