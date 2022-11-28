f8609566e14b06adf6009b2df4e5ced5f202ae54
noormesslmani <noor.messlmani@gmail.com>
2022-11-28 02:03:35 +0200

    Frontend: added schedules to BookingsContext

--- MeetALocal-rn/context/BookingsContext.js
+++ MeetALocal-rn/context/BookingsContext.js
@@ -3,5 +3,6 @@ import { createContext, useState} from "react";
 export const BookingsContext = createContext();
 export default function BookingsContextProvider({children}){
     const [bookings,setBookings]=useState([])
-    return <BookingsContext.Provider value={{bookings,setBookings}}>{children}</BookingsContext.Provider>;
+    const [schedules,setSchedules]=useState([])
+    return <BookingsContext.Provider value={{bookings,setBookings, schedules,setSchedules}}>{children}</BookingsContext.Provider>;
 }
