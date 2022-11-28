badc872480b76b414f2b80ba72e31d02b705cb5e
noormesslmani <noor.messlmani@gmail.com>
2022-11-28 03:54:09 +0200

    Frontend: added posts to EventsContextProvider

--- MeetALocal-rn/context/EventsContext.js
+++ MeetALocal-rn/context/EventsContext.js
@@ -3,5 +3,6 @@ import { createContext, useState} from "react";
 export const EventsContext = createContext();
 export default function EventsContextProvider({children}){
     const [events,setEvents]=useState([])
-    return <EventsContext.Provider value={{events,setEvents}}>{children}</EventsContext.Provider>;
+    const [posts,setPosts]=useState([])
+    return <EventsContext.Provider value={{events,setEvents, posts,setPosts}}>{children}</EventsContext.Provider>;
 }
